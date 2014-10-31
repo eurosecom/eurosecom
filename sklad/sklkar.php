bba5677f551c665413f773f414083347ef873347
eurosecom <eurosecom1@gmail.com>
2014-10-31 11:38:51 +0100

    SKLAD netlačí skladové karty do PDF
    #40
    18.06.2014 sa niečo prerábalo v sklkar.php, nemáme záznam v GitHube

---
+++ sklad/sklkar.php
@@ -0,0 +1,409 @@
+<HTML>
+<?php
+
+// celkovy zaciatok dokumentu
+       do
+       {
+$sys = 'SKL';
+$urov = 1000;
+$uziv = include("../uziv.php");
+if( !$uziv ) exit;
+
+if(!isset($kli_vxcf)) $kli_vxcf = 1;
+
+  require_once("../pswd/password.php");
+  @$spojeni = mysql_connect($mysqlhost, $mysqluser, $mysqlpasswd);
+  if (!$spojeni):
+    echo "Spojenie so serverom nedostupne.";
+    exit;
+  endif;
+  mysql_select_db($mysqldb);
+
+$sql = "SELECT * FROM F$kli_vxcf"."_vtvall";
+$vysledok = mysql_query($sql);
+if (!$vysledok):
+$vtvall = include("../cis/vtvall.php");
+endif;
+$sql = "SELECT * FROM F$kli_vxcf"."_vtvskl";
+$vysledok = mysql_query($sql);
+if (!$vysledok):
+$vtvskl = include("../sklad/vtvskl.php");
+endif;
+//    echo "VXCF $kli_vxcf Vume $kli_vume";
+
+// cislo operacie
+$copern = 1*$_REQUEST['copern'];
+
+$h_cis = $_REQUEST['h_cis'];
+$h_nat = $_REQUEST['h_nat'];
+$h_skl = $_REQUEST['h_skl'];
+
+$citfir = include("../cis/citaj_fir.php");
+
+
+//pozri do sklfak a oprav nulove cen
+if( $fir_xsk04 == 1 ) {
+
+
+$dsqlt = "DROP TABLE F$kli_vxcf"."_sklzaspriemer ";
+$dsql = mysql_query("$dsqlt");
+
+$kli_vxcfskl=$kli_vxcf;
+$priemer = include("sklzaspriemer.php");
+
+
+$sqlttxc = "SELECT * FROM F$kli_vxcf"."_sklfak WHERE cen = 0  ";
+$tovxc = mysql_query("$sqlttxc");
+$tvpolxc = mysql_num_rows($tovxc);
+
+
+if( $kli_uzid > 0 )
+ {
+
+$sqlttx = "SELECT * FROM F$kli_vxcf"."_sklcis WHERE tl3 = 1  ";
+$tovx = mysql_query("$sqlttx");
+$tvpolx = mysql_num_rows($tovx);
+          
+$ix=0;
+
+  while ($ix <= $tvpolx )
+  {
+
+  if (@$zaznam=mysql_data_seek($tovx,$ix))
+{
+$riadokx=mysql_fetch_object($tovx);
+
+$cenazprijmu=0;
+$poslhh = "SELECT * FROM F$kli_vxcf"."_sklpri WHERE cis = $riadokx->cis ORDER BY dat DESC ";
+$posl = mysql_query("$poslhh"); 
+  if (@$zaznam=mysql_data_seek($posl,0))
+  {
+  $posled=mysql_fetch_object($posl);
+  $cenazprijmu = 1*$posled->cen;
+  $cis = $posled->cis;
+  }
+
+$cenahranica1=0.4*$cenazprijmu; $cenahranica2=1.5*$cenazprijmu;
+if( $_SERVER['SERVER_NAME'] == "www.mlynzahorie.sk" ) { $cenahranica1=0.9*$cenazprijmu; $cenahranica2=1.1*$cenazprijmu; }
+
+if( $cenazprijmu > 0 )
+  {
+
+$dsqlt = "UPDATE F$kli_vxcf"."_sklzaspriemer SET cen=$cenazprijmu WHERE cis = $riadokx->cis AND ( cen < $cenahranica1 OR cen > $cenahranica2 ) ";
+$dsql = mysql_query("$dsqlt");
+  }
+
+}
+$ix=$ix+1;
+  }
+
+ }
+//koniec ak kliuzid > 0
+
+$dsqlt = "UPDATE F$kli_vxcf"."_sklfak,F$kli_vxcf"."_sklzaspriemer SET F$kli_vxcf"."_sklfak.cen=F$kli_vxcf"."_sklzaspriemer.cen ".
+" WHERE F$kli_vxcf"."_sklfak.cis=F$kli_vxcf"."_sklzaspriemer.cis AND F$kli_vxcf"."_sklfak.cen=0 ";
+$dsql = mysql_query("$dsqlt");
+
+if( $tvpolxc > 0 )
+  {
+$dsqlt = "DROP TABLE F$kli_vxcf"."_sklzaspriemer ";
+$dsql = mysql_query("$dsqlt");
+
+$kli_vxcfskl=$kli_vxcf;
+$priemer = include("sklzaspriemer.php");
+  }
+                      }
+//koniec pozri do sklfak a oprav nulove cen
+
+
+
+//urob pracovnu sklprc zjednotenie pri,vyd,pre,poc
+
+$sqlt = 'DROP TABLE F'.$kli_vxcf.'_sklprc';
+$vysledok = mysql_query("$sqlt");
+$sqlt = 'DROP TABLE F'.$kli_vxcf.'_sklprc2';
+$vysledok = mysql_query("$sqlt");
+$sqlt = 'DROP TABLE F'.$kli_vxcf.'_sklprckar'.$kli_uzid;
+$vysledok = mysql_query("$sqlt");
+
+$sqlt = <<<sklprc
+(
+   drp         INT,
+   ume         FLOAT(8,4),
+   dat         DATE,
+   dok         INT,
+   skl         INT,
+   poh         INT,
+   ico         INT,
+   cis         DECIMAL(15,0),
+   mno         DECIMAL(10,3),
+   cen         DECIMAL(10,4),
+   zas         DECIMAL(10,3),
+   vdj         DECIMAL(10,3),
+   prj         DECIMAL(10,3)
+);
+sklprc;
+
+$vsql = 'CREATE TABLE F'.$kli_vxcf.'_sklprc'.$sqlt;
+$vytvor = mysql_query("$vsql");
+$vsql = 'CREATE TABLE F'.$kli_vxcf.'_sklprckar'.$kli_uzid.$sqlt;
+$vytvor = mysql_query("$vsql");
+
+
+//poc.stav
+$psume="1".$kli_vrok;
+$psdat=$kli_vrok."-01-01";
+//echo $psdat;
+
+$dsqlt = "INSERT INTO F$kli_vxcf"."_sklprc".
+" SELECT 0,'$psume','$psdat',1,skl,1,0,cis,mno,cen,(mno),'0',(mno) FROM F$kli_vxcf"."_sklpoc WHERE cis > 0".
+" ORDER BY skl,poh,cis,cen".
+"";
+$dsql = mysql_query("$dsqlt");
+//prijem 
+$dsqlt = "INSERT INTO F$kli_vxcf"."_sklprc".
+" SELECT 1,ume,dat,dok,skl,poh,ico,cis,mno,cen,(mno),'0',(mno) FROM F$kli_vxcf"."_sklpri WHERE cis > 0".
+" ORDER BY skl,poh,cis,cen".
+"";
+$dsql = mysql_query("$dsqlt");
+//vydaj 
+$dsqlt = "INSERT INTO F$kli_vxcf"."_sklprc".
+" SELECT 2,ume,dat,dok,skl,poh,ico,cis,mno,cen,-(mno),(mno),'0' FROM F$kli_vxcf"."_sklvyd WHERE cis > 0".
+" ORDER BY skl,poh,cis,cen".
+"";
+$dsql = mysql_query("$dsqlt");
+//faktury 
+$dsqlt = "INSERT INTO F$kli_vxcf"."_sklprc".
+" SELECT 3,ume,dat,dok,skl,poh,ico,cis,mno,cen,-(mno),(mno),'0' FROM F$kli_vxcf"."_sklfak WHERE cis > 0".
+" ORDER BY skl,poh,cis,cen".
+"";
+$dsql = mysql_query("$dsqlt");
+//presun- 
+$dsqlt = "INSERT INTO F$kli_vxcf"."_sklprc".
+" SELECT 4,ume,dat,dok,skl,poh,0,cis,mno,cen,-(mno),(mno),'0' FROM F$kli_vxcf"."_sklpre WHERE cis > 0".
+" ORDER BY skl,poh,cis,cen".
+"";
+$dsql = mysql_query("$dsqlt");
+//presun+ 
+$dsqlt = "INSERT INTO F$kli_vxcf"."_sklprc".
+" SELECT 4,ume,dat,dok,sk2,poh,0,cis,mno,cen,(mno),'0',(mno) FROM F$kli_vxcf"."_sklpre WHERE cis > 0".
+" ORDER BY skl,poh,cis,cen".
+"";
+$dsql = mysql_query("$dsqlt");
+
+
+//group za skl,poh,cis,cen
+$dsqlt = "INSERT INTO F$kli_vxcf"."_sklprckar".$kli_uzid.
+" SELECT drp,ume,dat,dok,skl,poh,ico,cis,mno,cen,SUM(zas),SUM(vdj),SUM(prj) FROM F$kli_vxcf"."_sklprc ".
+" GROUP BY skl,poh,cis,cen,dat,dok".
+"";
+$dsql = mysql_query("$dsqlt");
+
+//zmaz pracovnu sklprc
+$sqlt = 'DROP TABLE F'.$kli_vxcf.'_sklprc';
+$vysledok = mysql_query("$sqlt");
+
+
+?>
+<HEAD>
+<META http-equiv="Content-Type" content="text/html; charset=cp1250">
+  <link type="text/css" rel="stylesheet" href="../css/styl.css">
+<title>Skladové karty</title>
+  <style type="text/css">
+
+  </style>
+<SCRIPT Language="JavaScript" Src="../js/cookies.js">
+</SCRIPT>
+
+<script type="text/javascript" src="../ajax/skl_kar_xml.js"></script>
+<script type="text/javascript" src="../ajax/skl_karta_xml.js"></script>
+
+<script type="text/javascript">
+//posuny Enter[[[[[[[[[[[
+
+
+
+function SluEnter(e)
+                {
+  var k = (navigator.appName=="Netscape") ? e : event.keyCode; // kód stlaèenej klávesy
+
+  if(k == 13 ){
+        if( document.forms.forms1.h_slu.value != ''  )
+        {
+        New.style.display="none";
+        mySklkarelement.style.display='none';
+        document.forms.forms1.h_nsl.value = '';
+        volajSklkar();
+        }      
+
+        if( document.forms1.h_slu.value == "" ) {  document.forms1.h_nsl.focus(); }
+
+              }
+                }
+
+
+function NslEnter(e)
+                {
+  var k = (navigator.appName=="Netscape") ? e : event.keyCode; // kód stlaèenej klávesy
+
+  if(k == 13 ){
+        if(document.forms.forms1.h_nsl.value != '' )
+        {
+        New.style.display="none";
+        mySklkarelement.style.display='none';
+        document.forms1.h_slu.value = ""; 
+        volajSklkar();
+        }   
+
+        if( document.forms1.h_nsl.value == "" ) { document.forms1.h_slu.focus(); }
+
+              }
+                }
+
+//co urobi po potvrdeni ok z tabulky sklkar
+function vykonajSklkar(slu,nazov,dph,mer,cena,zas)
+                {
+        document.forms.forms1.h_slu.value = slu;
+        document.forms.forms1.h_nsl.value = nazov;
+        document.forms.forms1.h_cen.value = cena;
+        mySklkarelement.style.display='none';
+        document.forms.forms1.h_nsl.focus();
+        document.forms.forms1.h_nsl.select();
+        urobSklkar();
+        mySklkarelement.style.display='';
+                }
+
+
+function Len1Sklkar()
+                    {
+        document.forms.forms1.h_nsl.focus();
+        document.forms.forms1.h_nsl.select();
+        urobSklkar();
+        mySklkarelement.style.display='';
+                    }
+
+function Len0Sklkar()
+                    {
+        New.style.display="";
+        document.forms.forms1.h_nsl.focus();
+        document.forms.forms1.h_nsl.select();
+                    }
+
+
+function VyberVstup()
+                {
+        document.forms.forms1.h_nsl.focus();
+        document.forms.forms1.h_nsl.select(); 
+                }
+
+function TlacJednejKarty()
+                {
+
+var h_skl = document.forms.forms1.h_skl.value;
+var h_cis = document.forms.forms1.h_slu.value;
+  var h_min = 0;
+  if( document.formx1.h_min.checked ) h_min=1;
+
+window.open('../sklad/sklkarta_pdf.php?copern=10&h_skl=' + h_skl + '&h_cis=' + h_cis + '&h_min=' + h_min + '&drupoh=<?php echo $drupoh; ?>', '_blank', 'width=800, height=900, top=0, left=10, status=yes, resizable=yes, scrollbars=yes'  );
+                }
+
+
+function TlacVsetkyKarty()
+                {
+
+var h_skl = document.forms.forms1.h_skl.value;
+var h_cis = document.forms.forms1.h_slu.value;
+  var h_min = 0;
+  if( document.formx1.h_min.checked ) h_min=1;
+
+window.open('../sklad/sklkarta_pdf.php?copern=20&h_skl=' + h_skl + '&h_cis=' + h_cis + '&h_min=' + h_min + '&drupoh=<?php echo $drupoh; ?>', '_blank', 'width=800, height=900, top=0, left=10, status=yes, resizable=yes, scrollbars=yes'  );
+                }
+
+  </script>
+</HEAD>
+<BODY class="white" id="white" onload="VyberVstup();" >
+
+
+<table class="h2" width="100%" >
+<tr>
+<td>EuroSecom  -  Skladové karty
+ <img src='../obr/info.png' width=12 height=12 border=0 alt="EnterNext = v tomto formuláry po zadaní hodnoty položky a stlaèení Enter program prejde na vstup ïalšej položky">
+
+</td>
+<td align="right"><span class="login"><?php echo "UME $kli_vume FIR$kli_vxcf-$kli_nxcf  login: $kli_uzmeno $kli_uzprie / $kli_uzid ";?></span></td>
+</tr>
+</table>
+<br />
+
+
+
+<table class="fmenu" width="100%" >
+<FORM name="formx1" class="obyc" method="post" action="#" >
+<tr>
+<td class="hmenu" width="20%">
+ <input type="checkbox" name="h_min" value="1" /> Minulý rok
+</tr>
+</FORM>
+
+<tr>
+<td class="hmenu" width="20%">Sklad
+<td class="hmenu" width="10%">Èíslo materiálu
+<td class="hmenu" width="30%">Názov materiálu
+<td class="hmenu" width="10%">Nákupná cena
+<td class="hmenu" width="30%">
+<input type='image' src='../obr/tlac.png' width=15 height=15 border=0 alt='Tlaè skladovej karty pre vybraný materiál'
+ onclick="TlacJednejKarty();" >
+<input type='image' src='../obr/tlac.png' width=20 height=20 border=0 alt='Tlaè všetky skladové karty'
+ onclick="TlacVsetkyKarty();" >
+</tr>
+<FORM name="forms1" class="obyc" method="post" action="#" >
+<tr>
+<td class="hmenu">
+<?php
+$sql = mysql_query("SELECT skl,nas FROM F$kli_vxcf"."_skl ORDER BY skl");
+?>
+<select size="1" name="h_skl" id="h_skl" >
+<option value="" >Všetky sklady</option>
+<?php while($zaznam=mysql_fetch_array($sql)):?>
+<option value="<?php echo $zaznam["skl"];?>" >
+<?php 
+$polmen = $zaznam["nas"];
+$poltxt = SubStr($polmen,0,20);
+?>
+<?php echo $zaznam["skl"];?> - <?php echo $poltxt;?></option>
+<?php endwhile;?>
+
+<td class="hmenu"><input type="text" name="h_slu" id="h_slu" size="10" 
+onclick=" mySklkarelement.style.display='none'; New.style.display='none'; document.forms.forms1.h_cen.value = '';"
+ onKeyDown="return SluEnter(event.which)"/>
+
+<td class="hmenu"><input type="text" name="h_nsl" id="h_nsl" size="50" value="<?php echo $h_nsl;?>" 
+onclick="mySklkarelement.style.display='none'; New.style.display='none'; document.forms.forms1.h_cen.value = '';"
+ onKeyDown="return NslEnter(event.which)" /> 
+
+<img src='../obr/hladaj.png' border="0" onclick="mySklkarelement.style.display='none'; New.style.display='none'; volajSklkar();"
+ title="H¾adaj zadané èíslo alebo názov materiálu" >
+
+
+<td class="hmenu"><input type="text" name="h_cen" id="h_cen" size="50" disabled="disabled" /> 
+
+</tr>
+
+<td class="obyc" align="right"><INPUT type="reset" onclick="mySklkarelement.style.display='none'; New.style.display='none'; document.forms.forms1.h_nsl.focus();"
+ id="resetp" name="resetp" value="Vymaza" ></td>
+
+</FORM>
+</table>
+
+<div id="mySklkarelement"></div>
+
+<span id="New" style="display:none; width:100%; align:center; font-family:bold; font-weight:bold; background-color:lime; color:black;">
+ Nepoznám položku CIS v žiadnom sklade v celom èíselníku materiálu , h¾adajte pod¾a názvu</span>
+
+<?php
+
+// celkovy koniec dokumentu
+       } while (false);
+?>
+</BODY>
+</HTML>
---
+++ sklad/sklkarta_pdf.php
@@ -0,0 +1,350 @@
+<HTML>
+<?php
+
+// celkovy zaciatok dokumentu
+       do
+       {
+$sys = 'SKL';
+$urov = 2000;
+$clsm = 920;
+$uziv = include("../uziv.php");
+if( !$uziv ) exit;
+
+// cislo operacie
+$copern = 1*$_REQUEST['copern'];
+$drupoh = 1*$_REQUEST['drupoh'];
+
+$h_skl = 1*$_REQUEST['h_skl'];
+$h_cis = 1*$_REQUEST['h_cis'];
+$h_min = 1*$_REQUEST['h_min'];
+
+  require_once("../pswd/password.php");
+  @$spojeni = mysql_connect($mysqlhost, $mysqluser, $mysqlpasswd);
+  if (!$spojeni):
+    echo "Spojenie so serverom nedostupne.";
+    exit;
+  endif;
+  mysql_select_db($mysqldb);
+
+
+$pole = explode(".", $kli_vume);
+$kli_vmes=$pole[0];
+$kli_vrok=$pole[1];
+
+$citnas = include("../cis/citaj_nas.php");
+$citfir = include("../cis/citaj_fir.php");
+$mena1 = $fir_mena1;
+$mena2 = $fir_mena2;
+$kurz12 = $fir_kurz12;
+
+//datumove funkcie
+$sDat = include("../funkcie/dat_sk_us.php");
+
+//tlacove okno
+$tlcuwin="width=700, height=' + vyskawin + ', top=0, left=200, status=yes, resizable=yes, scrollbars=yes, menubar=yes, toolbar=yes";
+$tlcswin="width=980, height=' + vyskawin + ', top=0, left=20, status=yes, resizable=yes, scrollbars=yes, menubar=yes, toolbar=yes";
+$tlcvwin="width=1020, height=' + vyskawin + ', top=0, left=20, status=yes, resizable=yes, scrollbars=yes, menubar=yes, toolbar=yes";
+$uliscwin="width=' + sirkawic + ', height=' + vyskawic + ', top=0, left=0, status=yes, resizable=yes, scrollbars=yes, menubar=no, toolbar=no";
+
+$databaza="";
+$mintext="";
+if( $h_min == 1 )
+{
+$kli_vxcf=$fir_allx11;
+$mintext=" - minulý rok ";
+
+
+if( $kli_vrok > 2010 )
+  {
+if (File_Exists ("../pswd/oddelena2010db2011.php")) { $databaza=$mysqldb2010."."; }
+  }
+
+
+}
+
+
+?>
+<HEAD>
+<META http-equiv="Content-Type" content="text/html; charset=Windows 1250">
+  <link type="text/css" rel="stylesheet" href="../css/styl.css">
+<title>Skladová karta PDF</title>
+  <style type="text/css">
+    #Okno{ display: none; cursor: hand; width: 150px;
+             position: absolute; top: 0; left: 0;
+             border: "1 solid";
+             background-color: "#f0f8ff";
+             border-top-color: "blue";
+             border-left-color: "blue";
+             border-right-color: "blue";
+             border-bottom-color: "blue";
+             font-size: 8pt; font-family: Arial;
+           }
+  </style>
+
+<script type="text/javascript">
+//sirka a vyska okna
+var sirkawin = screen.width-10;
+var vyskawin = screen.height-175;
+var vyskawic = screen.height;
+var sirkawic = screen.width-10;
+    
+
+
+
+</script>
+</HEAD>
+<BODY class="white" >
+
+
+<table class="h2" width="100%" >
+<tr>
+<td>EuroSecom  -  Skladová karta 
+
+</td>
+<td align="right"><span class="login"><?php echo "UME $kli_vume FIR$kli_vxcf-$kli_nxcf  login: $kli_uzmeno $kli_uzprie / $kli_uzid ";?></span></td>
+</tr>
+</table>
+<br />
+
+<?php
+if (File_Exists ("../tmp/sklkarta$kli_uzid.pdf")) { $soubor = unlink("../tmp/sklkarta$kli_uzid.pdf"); }
+
+   define('FPDF_FONTPATH','../fpdf/font/');
+   require('../fpdf/fpdf.php');
+
+$pdf=new FPDF("L","mm","A4");
+$pdf->Open();
+$pdf->AddFont('arial','','arial.php');
+
+if ( $copern == 20 OR $copern == 10 )
+    {
+
+$vsql = 'DROP TABLE '.$databaza.'F'.$kli_vxcf.'_sklprcx'.$kli_uzid;
+$vytvor = mysql_query("$vsql");
+
+$sqlt = <<<sklprc
+(
+   pox         INT,
+   drp         INT,
+   ume         FLOAT(8,4),
+   dat         DATE,
+   dok         INT,
+   skl         INT,
+   poh         INT,
+   ico         INT,
+   cis         DECIMAL(15,0),
+   mno         DECIMAL(10,3),
+   cen         DECIMAL(10,4),
+   zas         DECIMAL(10,3),
+   vdj         DECIMAL(10,3),
+   prj         DECIMAL(10,3),
+   zoseur      DECIMAL(10,2)
+);
+sklprc;
+
+$vsql = 'CREATE TABLE '.$databaza.'F'.$kli_vxcf.'_sklprcx'.$kli_uzid.$sqlt;
+$vytvor = mysql_query("$vsql");
+
+$podmskl = "skl > 0";
+if( $h_skl > 0 ) $podmskl = "skl = ".$h_skl;
+$podmcis = "".$databaza."F$kli_vxcf"."_sklprc".$kli_uzid.".cis > 0";
+if( $copern == 10 ) $podmcis = "".$databaza."F$kli_vxcf"."_sklprc".$kli_uzid.".cis  = ".$h_cis;
+
+//zober zo sklprc
+$dsqlt = "INSERT INTO ".$databaza."F$kli_vxcf"."_sklprcx".$kli_uzid.
+" SELECT 0,drp,ume,dat,dok,skl,poh,ico,cis,mno,cen,zas,vdj,prj,0 FROM ".$databaza."F$kli_vxcf"."_sklprc".$kli_uzid." ".
+" WHERE $podmcis AND $podmskl ".
+"";
+$dsql = mysql_query("$dsqlt");
+
+$dsqlt = "UPDATE ".$databaza."F$kli_vxcf"."_sklprcx$kli_uzid SET zoseur=zas*cen WHERE pox >= 0 ";
+$dsql = mysql_query("$dsqlt");
+
+//group za cis
+$dsqlt = "INSERT INTO ".$databaza."F$kli_vxcf"."_sklprcx".$kli_uzid.
+" SELECT 8,drp,ume,dat,dok,skl,poh,ico,cis,mno,cen,SUM(zas),SUM(vdj),SUM(prj),SUM(zoseur) FROM ".$databaza."F$kli_vxcf"."_sklprcx".$kli_uzid." ".
+" GROUP BY cis".
+"";
+$dsql = mysql_query("$dsqlt");
+
+//group za vsetko
+$dsqlt = "INSERT INTO ".$databaza."F$kli_vxcf"."_sklprcx".$kli_uzid.
+" SELECT 9,drp,ume,dat,dok,skl,poh,ico,999999999999999,mno,cen,SUM(zas),SUM(vdj),SUM(prj),SUM(zoseur) FROM ".$databaza."F$kli_vxcf"."_sklprcx".$kli_uzid." WHERE pox = 0".
+" GROUP BY pox".
+"";
+$dsql = mysql_query("$dsqlt");
+
+
+    }
+//koniec pracovneho suboru zjednotenie
+
+
+if ( $copern == 20 OR $copern == 10 )
+  {
+$podmskl = "skl > 0";
+if( $h_skl > 0 ) $podmskl = "skl = ".$h_skl;
+$podmcis = "".$databaza."F$kli_vxcf"."_sklprcx".$kli_uzid.".cis > 0";
+if( $copern == 10 ) $podmcis = "".$databaza."F$kli_vxcf"."_sklprcx".$kli_uzid.".cis  = ".$h_cis;
+
+$sqltt = "SELECT * ".
+" FROM ".$databaza."F$kli_vxcf"."_sklprcx".$kli_uzid.
+" LEFT JOIN ".$databaza."F$kli_vxcf"."_sklcis".
+" ON ".$databaza."F$kli_vxcf"."_sklprcx".$kli_uzid.".cis=".$databaza."F$kli_vxcf"."_sklcis.cis".
+" LEFT JOIN ".$databaza."F$kli_vxcf"."_ico".
+" ON ".$databaza."F$kli_vxcf"."_sklprcx".$kli_uzid.".ico=".$databaza."F$kli_vxcf"."_ico.ico".
+" WHERE $podmcis AND $podmskl ORDER BY ".$databaza."F$kli_vxcf"."_sklprcx".$kli_uzid.".cis,pox,dat,dok,poh";
+
+  }
+
+
+
+//echo $sqltt;
+//exit;
+
+$tov = mysql_query("$sqltt");
+$tvpol = mysql_num_rows($tov);
+
+//exit;
+
+$zostatokeur=0;
+$zostatok=0;
+$new=0;
+$strana=0;
+$j=0;           
+$i=0;
+  while ($i <= $tvpol )
+  {
+
+  if (@$zaznam=mysql_data_seek($tov,$i))
+{
+$riadok=mysql_fetch_object($tov);
+
+
+//hlavicka strany
+if ( $j == 0 )
+     {
+
+$pdf->AddPage();
+
+$pdf->SetLeftMargin(15); 
+$pdf->SetTopMargin(15); 
+
+$strana=$strana+1;
+
+$pdf->SetFont('arial','',10);
+if ( $copern == 20 )  { $pdf->Cell(90,6,"Skladová karta $mintext","LTB",0,"L"); }
+if ( $copern == 10 )  { $pdf->Cell(90,6,"Skladová karta $mintext","LTB",0,"L"); }
+$pdf->Cell(0,6,"$kli_nxcf strana $strana","RTB",1,"R");
+
+$pdf->SetFont('arial','',6);
+
+
+$pdf->Cell(10,5,"SKL","1",0,"R");$pdf->Cell(20,5,"Dátum","1",0,"R");$pdf->Cell(20,5,"Doklad","1",0,"R");
+$pdf->Cell(20,5,"Pohyb","1",0,"R");$pdf->Cell(60,5,"Dodávate¾/Odberate¾","1",0,"L");
+$pdf->Cell(20,5,"Množstvo MJ","1",0,"R");$pdf->Cell(20,5,"Cena/MJ","1",0,"R");
+$pdf->Cell(20,5,"Príjem MJ","1",0,"R");$pdf->Cell(20,5,"Výdaj MJ","1",0,"R");
+$pdf->Cell(20,5,"Zostatok Eur","1",0,"R");$pdf->Cell(0,5,"Zostatok MJ","1",1,"R");
+
+$pdf->SetFont('arial','',9);
+$pdf->Cell(0,5,"Materiál $riadok->cis $riadok->nat v $riadok->mer ","1",1,"L");
+
+$new=0;
+     }
+//koniec hlavicky j=0
+
+
+
+if( $riadok->pox == 0 )
+{
+
+$pdf->SetFont('arial','',9);
+
+$zostatok=$zostatok+$riadok->zas;
+$zostatokeur=$zostatokeur+$riadok->zoseur;
+
+$Cislo=$zostatok+"";
+$szostatok=sprintf("%0.3f", $Cislo);
+$Cislo=$zostatokeur+"";
+$szostatokeur=sprintf("%0.2f", $Cislo);
+
+$dat_sk=SkDatum($riadok->dat);
+$prj=$riadok->prj;
+if( $riadok->prj == 0 ) $prj="";
+$vdj=$riadok->vdj;
+if( $riadok->vdj == 0 ) $vdj="";
+
+if( $new == 1 ) { $pdf->Cell(0,5,"Materiál $riadok->cis $riadok->nat v $riadok->mer ","1",1,"L"); $j=$j+1; $new=0; }
+
+$pdf->Cell(10,5,"$riadok->skl","0",0,"R");$pdf->Cell(20,5,"$dat_sk","0",0,"R");$pdf->Cell(20,5,"$riadok->dok","0",0,"R");
+$pdf->Cell(20,5,"$riadok->poh","0",0,"R");$pdf->Cell(60,5,"$riadok->nai","0",0,"L");
+$pdf->Cell(20,5,"$riadok->mno","0",0,"R");$pdf->Cell(20,5,"$riadok->cen","0",0,"R");
+$pdf->Cell(20,5,"$prj","0",0,"R");$pdf->Cell(20,5,"$vdj","0",0,"R");
+$pdf->Cell(20,5,"$szostatokeur","0",0,"R");$pdf->Cell(0,5,"$szostatok","0",1,"R");
+
+}
+
+if( $riadok->pox == 8 )
+{
+
+$pdf->SetFont('arial','',9);
+
+$pdf->Cell(210,5,"CELKOM Materiál $riadok->cis $riadok->nat v $riadok->mer ","LTB",0,"L");
+$pdf->Cell(20,5,"$riadok->zoseur","TB",0,"R");$pdf->Cell(0,5,"$riadok->zas","RTB",1,"R");
+
+$zostatok=0;
+$zostatokeur=0;
+$new=1;
+$pdf->Cell(0,5," ","0",1,"R");
+$j=$j+1;
+}
+
+
+if( $riadok->pox == 9 )
+{
+
+$pdf->SetFont('arial','',9);
+
+$pdf->Cell(210,5,"CELKOM všetko","LTB",0,"L");
+$pdf->Cell(20,5,"$riadok->zoseur","TB",0,"R");$pdf->Cell(0,5,"$riadok->zas","RTB",1,"R");
+$j=$j+1;
+}
+
+
+
+}
+$i = $i + 1;
+$j = $j + 1;
+if( $j > 30 ) $j=0;
+
+  }
+
+
+$pdf->Output("../tmp/sklkarta.$kli_uzid.pdf");
+
+
+$sqlt = 'DROP TABLE '.$databaza.'F'.$kli_vxcf.'_sklprc'.$kli_uzid;
+//$vysledok = mysql_query("$sqlt");
+$sqlt = 'DROP TABLE '.$databaza.'F'.$kli_vxcf.'_sklprcd'.$kli_uzid;
+//$vysledok = mysql_query("$sqlt");
+
+if( $h_min == 1 AND $copern == 20 )
+{
+$sqlt = 'DROP TABLE '.$databaza.'F'.$kli_vxcf.'_sklprc'.$kli_uzid;
+$vysledok = mysql_query("$sqlt");
+$vsql = "CREATE TABLE ".$databaza."F".$kli_vxcf."_sklprc".$kli_uzid." SELECT * FROM ".$databaza."F".$kli_vxcf."_sklprcx".$kli_uzid." ";
+$vytvor = mysql_query("$vsql");
+}
+?> 
+
+<script type="text/javascript">
+  var okno = window.open("../tmp/sklkarta.<?php echo $kli_uzid; ?>.pdf","_self");
+</script>
+
+?>
+
+<?php
+// celkovy koniec dokumentu
+       } while (false);
+?>
+</BODY>
+</HTML>
