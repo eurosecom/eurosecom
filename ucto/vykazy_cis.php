ecacce142966b4dec8dd27df93f1b6d89d43c1e3
61Stan <stanislav61@gmail.com>
2014-11-10 14:17:48 +0100

    Účtovná závierka mikro účt.jednotka
    #32
    - prerábka layout pre nastvenie výkazov múj
    
    Todo
    - vyčleniť $drupoh == 96 mimo základnej tabulky - od riadku 704
    - tool <a> v záhlaví musia byť vo form?

--- ucto/vykazy_cis.php
+++ ucto/vykazy_cis.php
@@ -1,12 +1,11 @@
+<!doctype html>
 <HTML>
 <?php
 $sys = 'UCT';
 $urov = 1000;
 $uziv = include("../uziv.php");
-if( !$uziv ) exit;
-
-if(!isset($kli_vxcf)) $kli_vxcf = 1;
-
+if ( !$uziv ) exit;
+if (!isset($kli_vxcf)) $kli_vxcf = 1;
        do
        {
 
@@ -23,8 +22,8 @@ $pole = explode(".", $kli_vume);
 $kli_vmes=$pole[0];
 $kli_vrok=$pole[1];
 
-if( $h_obdp == 0 ) $h_obdp=$kli_vmes;
-if( $h_obdk == 0 ) $h_obdk=$kli_vmes;
+if ( $h_obdp == 0 ) $h_obdp=$kli_vmes;
+if ( $h_obdk == 0 ) $h_obdk=$kli_vmes;
 
 require_once("../pswd/password.php");
 @$spojeni = mysql_connect($mysqlhost, $mysqluser, $mysqlpasswd);
@@ -144,10 +143,10 @@ $ulozene = mysql_query("$sql");
     {
 ?>
 <script type="text/javascript">
-if( !confirm ("Chcete naËÌtaù generovanie z minulÈho roka ?") )
+if ( !confirm ("Chcete naËÌtaù generovanie z minulÈho roka ?") ) //dopyt, Ëo je to za hl·öku?
          { window.close()  }
 else
-         { location.href='vykazy_cis.php?copern=4056&page=1&drupoh=<?php echo $drupoh; ?>'  }
+         { location.href='vykazy_cis.php?copern=4056&page=1&drupoh=<?php echo $drupoh; ?>' }
 </script>
 <?php
     }
@@ -156,28 +155,28 @@ else
     {
 
 $h_ycf=0;
-if( $fir_allx11 > 0 ) $h_ycf=1*$fir_allx11;
+if ( $fir_allx11 > 0 ) $h_ycf=1*$fir_allx11;
 
 $databaza="";
-if( $kli_vrok > 2010 )
+if ( $kli_vrok > 2010 )
 {
-if (File_Exists ("../pswd/oddelena2010db2011.php")) { $databaza=$mysqldb2010."."; }
+if ( File_Exists("../pswd/oddelena2010db2011.php") ) { $databaza=$mysqldb2010."."; }
 }
-if( $kli_vrok > 2011 )
+if ( $kli_vrok > 2011 )
 {
-if (File_Exists ("../pswd/oddelena2011db2012.php")) { $databaza=$mysqldb2011."."; }
+if ( File_Exists("../pswd/oddelena2011db2012.php") ) { $databaza=$mysqldb2011."."; }
 }
-if( $kli_vrok > 2012 )
+if ( $kli_vrok > 2012 )
 {
-if (File_Exists ("../pswd/oddelena2012db2013.php")) { $databaza=$mysqldb2012."."; }
+if ( File_Exists("../pswd/oddelena2012db2013.php") ) { $databaza=$mysqldb2012."."; }
 }
-if( $kli_vrok > 2013 )
+if ( $kli_vrok > 2013 )
 {
-if (File_Exists ("../pswd/oddelena2013db2014.php")) { $databaza=$mysqldb2013."."; }
+if ( File_Exists("../pswd/oddelena2013db2014.php") ) { $databaza=$mysqldb2013."."; }
 }
-if( $kli_vrok > 2014 )
+if ( $kli_vrok > 2014 )
 {
-if (File_Exists ("../pswd/oddelena2014db2015.php")) { $databaza=$mysqldb2014."."; }
+if ( File_Exists("../pswd/oddelena2014db2015.php") ) { $databaza=$mysqldb2014."."; }
 }
 
 
@@ -301,13 +300,9 @@ $ulozene = mysql_query("$sqult");
 }
      }
                      }
-
-
-$copern=308; 
+$copern=308;
     }
 //koniec nacitania standartneho generovania
-
-
 
 
 //vymazanie 
@@ -319,7 +314,7 @@ $zmazane = mysql_query("DELETE FROM F$kli_vxcf"."_$uctsys WHERE cpl='$cislo_cpl'
 $copern=308;
 if (!$zmazane):
 ?>
-<script type="text/javascript"> alert( " POLOéKA NEBOLA VYMAZAN¡ " ) </script>
+<script type="text/javascript"> alert( "POLOéKA NEBOLA VYMAZAN¡" ) </script>
 <?php
 endif;
 if ($zmazane):
@@ -354,7 +349,7 @@ $uprav=0;
 
 if (!$ulozene):
 ?>
-<script type="text/javascript"> alert( " POLOéKA NEBOLA SPR¡VNE ULOéEN¡ " ) </script>
+<script type="text/javascript"> alert( "POLOéKA NEBOLA SPR¡VNE ULOéEN¡" ) </script>
 <?php
 endif;
 if ($ulozene):
@@ -390,7 +385,7 @@ $copern=308;
 $uprav=0;
 if (!$ulozene):
 ?>
-<script type="text/javascript"> alert( " POLOéKA NEBOLA SPR¡VNE ULOéEN¡ " ) </script>
+<script type="text/javascript"> alert( "POLOéKA NEBOLA SPR¡VNE ULOéEN¡" ) </script>
 <?php
 endif;
 if ($ulozene):
@@ -428,183 +423,229 @@ $h_crs = $riadok->hod;
                   }
 
   }
-
 $cislo_cpl = strip_tags($_REQUEST['cislo_cpl']);
 $zmazane = mysql_query("DELETE FROM F$kli_vxcf"."_$uctsys WHERE cpl='$cislo_cpl' "); 
-
        }
-
 //koniec uprava nacitanie
-
-?> 
-
+?>
 <HEAD>
 <META http-equiv="Content-Type" content="text/html; charset=cp1250">
-  <link type="text/css" rel="stylesheet" href="../css/styl.css">
-<title>⁄prava ËÌselnÌkov</title>
-  <style type="text/css">
+ <link rel="stylesheet" href="../css/reset.css" type="text/css">
+ <link rel="stylesheet" href="../css/tlaciva.css" type="text/css">
+<title>EuroSecom - M⁄J z·vierka nastavenie</title>
+<style type="text/css">
+table.content-body {
+  width: 500px;
+  margin-left: 80px;
+/* background-color:#ececec; */
+}
+table.content-body thead th {
+  font-size: 11px;
+  text-align: left;
+  height: 14px;
+}
+table.content-body tbody td {
+  background-color: #ececec;
+  line-height: 18px;
+  border-bottom: 2px solid #fff;
+  border-right: 2px solid #fff;
+  font-size: 12px;
+  
+}
+table.content-body tfoot td {
+  height: 20px;
 
-  </style>
+}
+
+a.btn-edit, a.btn-cancel {
+  display: inline-block;
+  width: 16px;
+  height: 16px;
+  vertical-align: -4px;
+  opacity: 0.7;
+}
+a.btn-edit {
+  background-image: url(../obr/ikony/pencil3_blue_x16.png);
+  background-repeat: no-repeat;
+
+}
+a.btn-cancel {
+  background-image: url(../obr/ikony/xmark_red_x16.png);
+  background-repeat: no-repeat;
+
+}
+
+a.btn-edit:hover, a.btn-cancel:hover {
+  opacity: 1;
+}
+
+</style>
+
 <script type="text/javascript">
 //sirka a vyska okna
-var sirkawin = screen.width-10;
 var vyskawin = screen.height-175;
 
-//  Kontrola cisla
-    function KontrolaCisla(Vstup, Oznam)
-    {
-     if ( Vstup.value.search(/[^0-9]/g) != -1) { Vstup.value=Vstup.value.replace(",","."); }
-     if ( Vstup.value.search(/[^0-9]/g) != -1) { Oznam.style.display=""; }
-     else { Oznam.style.display="none"; }
-    }
+  function VyberVstup()
+  {
+  }
 
-//  Kontrola cisla desatinneho
-    function KontrolaDcisla(Vstup, Oznam)
-    {
-     if ( Vstup.value.search(/[^0-9.-]/g) != -1) { Vstup.value=Vstup.value.replace(",","."); }
-     if ( Vstup.value.search(/[^0-9.-]/g) != -1) { Oznam.style.display=""; }
-     else { Oznam.style.display="none"; }
-    }
+  function ObnovUI()
+  {
+<?php if ( $copern == 308 AND $uprav >= 0 AND $drupoh >= 91 ) { ?>
+   document.formv1.h_uce.value = '<?php echo "$h_uce";?>';
+   document.formv1.h_crs.value = '<?php echo "$h_crs";?>';
+<?php if ( $drupoh == 95 ) { ?>
+   document.formv1.h_ucd.value = '<?php echo "$h_ucd";?>';
+<?php                      } ?>
+<?php                                                         } ?>
+  }
 
-//  Kontrola datumu
-    function KontrolaDatum(Vstup, Oznam)
-    {
-     if ( Vstup.value.search(/[^0-9.]/g) != -1) { Vstup.value=Vstup.value.replace(",","."); }
-     if ( Vstup.value.search(/[^0-9.]/g) != -1) { Oznam.style.display=""; }
-     else { Oznam.style.display="none"; }
-    }
+  function Povol_uloz()
+  {
+  }
 
-
-
-
-    function VyberVstup()
-    {
-
-    }
-
-    function ObnovUI()
-    {
-
-    <?php if( $copern == 308 AND $uprav >= 0 AND $drupoh >= 91  ) { ?>
-    document.formv1.h_uce.value = '<?php echo "$h_uce";?>';
-    document.formv1.h_crs.value = '<?php echo "$h_crs";?>';
-    <?php if( $drupoh == 95  ) { ?>
-    document.formv1.h_ucd.value = '<?php echo "$h_ucd";?>';
-    <?php                      } ?>
-    <?php                                                         } ?>
-    }
-
-    function Povol_uloz()
-    {
-
-    }
-
-function ZmazPolozku(cpl)
-                {
-var cislo_cpl = cpl;
-
-window.open('../ucto/vykazy_cis.php?copern=316&page=1&sysx=UCT&cislo_cpl=' + cislo_cpl + '&drupoh=<?php echo $drupoh; ?>&uprav=0',
- '_self' );
-                }
-
-function UpravPolozku(cpl)
-                {
-var cislo_cpl = cpl;
-
-window.open('../ucto/vykazy_cis.php?copern=308&page=1&sysx=UCT&uprav_cpl=' + cislo_cpl + '&cislo_cpl=' + cislo_cpl + '&drupoh=<?php echo $drupoh; ?>&uprav=1',
- '_self' );
-                }
+  function ZmazPolozku(cpl)
+  {
+   var cislo_cpl = cpl;
+   window.open('../ucto/vykazy_cis.php?copern=316&page=1&sysx=UCT&cislo_cpl=' + cislo_cpl + '&drupoh=<?php echo $drupoh; ?>&uprav=0', '_self');
+  }
+  function UpravPolozku(cpl)
+  {
+   var cislo_cpl = cpl;
+   window.open('../ucto/vykazy_cis.php?copern=308&page=1&sysx=UCT&uprav_cpl=' + cislo_cpl + '&cislo_cpl=' + cislo_cpl + '&drupoh=<?php echo $drupoh; ?>&uprav=1', '_self');
+  }
   
 </script>
 </HEAD>
-<BODY class="white" id="white" onload="ObnovUI(); VyberVstup();" >
+<BODY id="white" onload="ObnovUI(); VyberVstup();">
+
+<div id="wrap-heading">
+ <table id="heading">
+  <tr>
+   <td class="ilogin">EuroSecom</td>
+   <td class="ilogin" align="right"><?php echo "<strong>UME</strong> $kli_vume&nbsp;&nbsp;<strong>FIR</strong> $kli_vxcf:$kli_nxcf&nbsp;&nbsp;<strong>login</strong> $kli_uzmeno $kli_uzprie / $kli_uzid";?></td>
+  </tr>
+  <tr>
+   <td class="header">⁄Ëtovn· z·vierka <span class="subheader">mikro ˙Ëtovnej jednotky</span> - nastavenie</td>
+   <td></td>
+  </tr>
+ </table>
+</div>
+
+<div id="content" >
 
 
 
-<table class="h2" width="100%" >
-<tr>
-<td>EuroSecom  -  
-<?php if( $drupoh == 91 ) { echo " Generovanie v˝kazu SUV MUJ 1-01 v.2014"; } ?>
-<?php if( $drupoh == 93 ) { echo " Bezprostredne predch·dzaj˙ce ˙ËtovnÈ obdobie v˝kazu SUV MUJ 1-01 v.2014"; } ?>
-<?php if( $drupoh == 92 ) { echo " Generovanie v˝kazu VZaS MUJ 2-01 v.2014"; } ?>
-<?php if( $drupoh == 94 ) { echo " Bezprostredne predch·dzaj˙ce ˙ËtovnÈ obdobie v˝kazu VZaS MUJ 2-01 v.2014"; } ?>
-<?php if( $drupoh == 95 ) { echo " Generovanie v˝kazu SUV MUJ 1-01 v.2014 AktÌva / PasÌva"; } ?>
-<?php if( $drupoh == 96 ) { echo " Nastavenie zaokr˙hlenia v˝kazu SUV MUJ 1-01 a VZaS MUJ 2-01 v.2014"; } ?>
-</td>
-<td align="right"><span class="login"><?php echo "UME $kli_vume FIR$kli_vxcf-$kli_nxcf  login: $kli_uzmeno $kli_uzprie / $kli_uzid ";?></span></td>
-</tr>
-</table>
-<br />
+
+<?php
+$clas1="noactive"; $clas2="noactive"; if ( $strana == 1 ) $clas1="active"; if ( $strana == 2 ) $clas2="active"; if ( $strana == 3 ) $clas3="active";
+
+?>
+<div class="wrap-content-navbar" style="float:right; width:100%; background-color:lightblue; line-height:20px;">
+ <h2 class="" style="font-family:arial; font-size:14px; float:left; font-weight: bold; ">
+<?php
+if ( $drupoh == 91 ) { echo "S˙vaha - Generovanie"; }
+if ( $drupoh == 93 ) { echo "S˙vaha - Predch·dzaj˙ce ˙ËtovnÈ obdobie"; }
+if ( $drupoh == 92 ) { echo "VZaS - Generovanie"; }
+if ( $drupoh == 94 ) { echo "VZaS - Predch·dzaj˙ce ˙ËtovnÈ obdobie"; }
+if ( $drupoh == 95 ) { echo "S˙vaha - generovanie v˝kazu AktÌva / PasÌva"; }
+if ( $drupoh == 96 ) { echo "S˙vaha a VZaS - Nastavenie zaokr˙hlenia"; }
+?>
+ </h2>
+
+
+
+
+</div>
+
+
+<div class="" style="background-color:white;">
+
+
+
+
+
+
+
+
+
 
 
 
 
 <?php
 ////////////////////////////////////////////////////////////////uprava vykazu
-if( $drupoh >= 91 )           
+if ( $drupoh >= 91 )
 {
 
-if( $gener == 1 ) { 
+if ( $gener == 1 ) {
 $sqltt = "SELECT * FROM F$kli_vxcf"."_$uctsys WHERE cpl > 0 ORDER BY cpl";
-                  }
-if( $minul == 1 ) { 
+                   }
+if ( $minul == 1 ) {
 $sqltt = "SELECT cpl, dok AS uce, hod AS crs FROM F$kli_vxcf"."_$uctsys WHERE cpl > 0 ORDER BY cpl";
-                  }
-if( $drupoh == 95 ) { 
+                   }
+if ( $drupoh == 95 ) {
 $sqltt = "SELECT cpl, dok AS uce, ucm AS crs, ucd FROM F$kli_vxcf"."_$uctsys WHERE cpl > 0 ORDER BY cpl";
-                    }
+                     }
 $sql = mysql_query("$sqltt");
 //echo $sqltt;
 
 // celkom poloziek
 $cpol = mysql_num_rows($sql);
 $i = 0;
-
 ?>
-<table class="vstup" width="100%" >
-<FORM name="formv1" class="obyc" method="post" action="vykazy_cis.php?copern=315&uprav=<?php echo $uprav;?>&drupoh=<?php echo $drupoh;?>" >
-<tr>
-<?php if( $gener == 1 ) { ?>
-<?php if( $drupoh != 95 AND $drupoh != 96 ) { ?>
-<td class="hmenu" width="10%" >⁄Ëet</td>
-<td class="hmenu" width="10%" >Riadok</td>
-<td class="hmenu" width="10%" > </td>
-<?php                     } ?>
-<?php if( $drupoh == 95 ) { ?>
-<td class="hmenu" width="10%" >⁄Ëet</td>
-<td class="hmenu" width="10%" >AktÌva</td>
-<td class="hmenu" width="10%" >PasÌva</td>
-<?php                     } ?>
-<?php if( $drupoh == 96 ) { ?>
-<td class="hmenu" width="10%" >ËrSUV
- <img src='../obr/info.png' width=12 height=12 border=0 title="»Ìslo riadku aktÌv v S˙vahe kam m· program z˙Ëtovaù rozdiel HV po zaokr˙hlenÌ S˙vahy, napr. Ë. 15">
-</td>
-<td class="hmenu" width="10%" >ËrVZS
- <img src='../obr/info.png' width=12 height=12 border=0 title="»Ìslo n·kladovÈho riadku vo V˝kaze ziskov kam m· program z˙Ëtovaù rozdiel HV po zaokr˙hlenÌ S˙vahy a VZaS, napr. Ë. 17">
-</td>
-<td class="hmenu" width="10%" > </td>
-<?php                     } ?>
-<?php                   } ?>
-<?php if( $minul == 1 ) { ?>
-<td class="hmenu" width="10%" >Riadok</td>
-<td class="hmenu" width="10%" >Hodnota</td>
-<td class="hmenu" width="10%" > </td>
-<?php                   } ?>
-<td class="hmenu" width="10%" > </td>
-<td class="hmenu" width="10%" >
-<a href='vykazy_cis.php?drupoh=<?php echo $drupoh; ?>&copern=4055&page=1'>
-<img src='../obr/import.png' width=20 height=15 border=0 title="NaËÌtaj z minulÈho roka"></a>
-</td>
-<td class="hmenu" width="10%" >
-<a href='vykazy_cis.php?drupoh=<?php echo $drupoh; ?>&copern=155&page=1'>
-<img src='../obr/orig.png' width=20 height=15 border=0 title="Nastav ötandartn˝ ËÌselnÌk"></a>
-</td>
-<td class="hmenu" width="10%" > </td>
-<td class="hmenu" width="10%" > </td>
-<td class="hmenu" width="10%" > </td>
-<td class="hmenu" width="10%" > </td>
-</tr>
+<table class="content-body" style="">
+<FORM name="formv1" method="post" action="vykazy_cis.php?copern=315&uprav=<?php echo $uprav;?>&drupoh=<?php echo $drupoh;?>">
 
+<thead>
+<tr>
+ <th style="width:25%;"></th>
+ <th style="width:25%;"></th>
+ <th style="width:25%;"></th>
+ <th style="width:25%;"></th>
+</tr>
+<tr>
+<?php
+if ( $gener == 1 )
+{
+?>
+<?php if ( $drupoh != 95 AND $drupoh != 96 ) { ?>
+ <th>⁄Ëet</th>
+ <th colspan="2">Riadok</th>
+<?php                                        } ?>
+<?php if ( $drupoh == 95 ) { ?>
+ <th>⁄Ëet</th>
+ <th>AktÌva</th>
+ <th>PasÌva</th>
+<?php                      } ?>
+
+<?php if ( $drupoh == 96 ) { ?>
+ <th>
+ ËrSUV <img src='../obr/info.png' width=12 height=12 border=0 title="»Ìslo riadku aktÌv v S˙vahe kam m· program z˙Ëtovaù rozdiel HV po zaokr˙hlenÌ S˙vahy, napr. Ë. 15">
+ </th> <!-- dopyt, spoloËn˝ obr·zok -->
+ <th>
+ËrVZS <img src='../obr/info.png' width=12 height=12 border=0 title="»Ìslo n·kladovÈho riadku vo V˝kaze ziskov kam m· program z˙Ëtovaù rozdiel HV po zaokr˙hlenÌ S˙vahy a VZaS, napr. Ë. 17">
+</th>
+
+<?php                     } ?>
+<?php
+}
+?>
+
+<?php if ( $minul == 1 ) { ?>
+ <th>Riadok</th>
+ <th colspan="2">Hodnota</th>
+<?php                    } ?>
+ <th>
+  <a href='vykazy_cis.php?drupoh=<?php echo $drupoh; ?>&copern=4055&page=1' style="display:none;"> <!-- dopyt, doprava na liötu spraviù tlaËidlo "naËÌtaù" -->
+   <img src='../obr/import.png' width=20 height=15 border=0 title="NaËÌtaj z minulÈho roka">
+  </a>
+  <a href='vykazy_cis.php?drupoh=<?php echo $drupoh; ?>&copern=155&page=1' style="display:none;">
+   <img src='../obr/orig.png' width=20 height=15 border=0 title="Nastav ötandartn˝ ËÌselnÌk">
+  </a>
+ </th>
+</tr>
+</thead>
 <?php
    while ($i <= $cpol )
    {
@@ -614,57 +655,70 @@ $i = 0;
   {
 $riadok=mysql_fetch_object($sql);
 ?>
+<tbody>
 <tr>
+ <td><?php echo $riadok->uce;?></td>
+ <td style="text-align:right;"><?php echo $riadok->crs;?></td>
 
-<td class="fmenu" ><?php echo $riadok->uce;?></td>
-<td class="fmenu" align="right" ><?php echo $riadok->crs;?></td>
-<?php if( $drupoh == 95 ) { ?>
-<td class="fmenu" align="right" ><?php echo $riadok->ucd;?></td>
-<?php                     } ?>
-<td class="fmenu" >
-<a href="#" onClick="UpravPolozku(<?php echo $riadok->cpl;?>);">
-<img src='../obr/uprav.png' width=15 height=10 border=0 title='Upraviù riadok' ></a>
+<?php if ( $drupoh == 95 ) { ?>
+ <td style="text-align:right;"><?php echo $riadok->ucd;?></td>
+<?php                      } ?>
 
-<td class="fmenu" >
-<a href="#" onClick="ZmazPolozku(<?php echo $riadok->cpl;?>);">
-<img src='../obr/zmaz.png' width=15 height=10 border=0 title='Vymazaù riadok' ></a>
-
-</td>
+ <td style="text-align:center;">
+  <a href="#" onclick="UpravPolozku(<?php echo $riadok->cpl;?>);" title="Upraviù" class="btn-edit"></a>
+  <a href="#" onclick="ZmazPolozku(<?php echo $riadok->cpl;?>);" title="Vymazaù" class="btn-cancel" ></a>
+ </td>
 </tr>
+</tbody>
 <?php
   }
 $i = $i + 1;
    }
 ?>
+<tfoot>
+<tr>
+ <td>
+  <input type="text" name="h_uce" id="h_uce" size="7" onclick=" " onKeyDown=" "  onkeyup=" " />
+ </td>
+ <td>
+  <input type="text" name="h_crs" id="h_crs" size="7" onclick=" " onKeyDown=" "  onkeyup=" "/>
+ </td>
+<?php if ( $drupoh == 95 ) { ?>
+ <td>
+  <input type="text" name="h_ucd" id="h_ucd" size="7" onclick=" " onKeyDown=" "  onkeyup=" "/>
+ </td>
+<?php                      } ?>
+</tr>
+
 
 
 <tr>
-
-<td class="hmenu"><input type="text" name="h_uce" id="h_uce" size="7" onclick=" "
- onKeyDown=" "  onkeyup=" " />
-
-<td class="hmenu"><input type="text" name="h_crs" id="h_crs" size="7" onclick=" "
- onKeyDown=" "  onkeyup=" "/>
-
-<?php if( $drupoh == 95 ) { ?>
-<td class="hmenu"><input type="text" name="h_ucd" id="h_ucd" size="7" onclick=" "
- onKeyDown=" "  onkeyup=" "/>
-<?php                     } ?>
+ <td></td>
+ <td colspan="3" style="">
+  <input type="submit" id="uloz" name="uloz" value="Uloûiù" >
+ </td>
 </tr>
-
-<tr>
-<td class="obyc" colspan="2"><INPUT type="submit" id="uloz" name="uloz" value="Uloûiù" ></td>
-</tr>
+</tfoot>
 </table>
+
+<!-- dopyt, sem pÙjde drupoh 96 -->
+
+
+</FORM>
 
 <?php
 }
 ////////////////////////////////////////////////////////////////koniec uprava vykazu
 ?>
 
-<?php
-// celkovy koniec dokumentu
+ </div> <!-- koniec .content-body -->
 
+
+</div> <!-- koniec  #content -->
+
+
+<?php
+//celkovy koniec dokumentu
        } while (false);
 ?>
 </BODY>
