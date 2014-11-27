5c2388284059512ad62f5213fbb104365fe9eb00
eurosecom <eurosecom1@gmail.com>
2014-11-27 15:17:47 +0100

    Účtovná závierka mikro účt. jednotka
    #32
    Kostra Poznamky MUJ s upravou a nadpisami textov.
    
    TODO
    -vlozit z poznamky_po2013.php tabulku s hodnotami ac11 az ac32 pod
    nadpis pocet zamestnancov
    -preskumam dalsie tabulky co sa budu prenasat z POD2013 moc ich nebude
    podmienene zavazky...

--- ucto/poznamky_muj2014.php
+++ ucto/poznamky_muj2014.php
@@ -99,12 +99,7 @@ $vysledek = mysql_query("$sql");
 //koniec vytvor tabulku textov v databaze
 
 //vytvor tabulku v databaze
-$sql = "SELECT w1 FROM F$kli_vxcf"."_poznamky_muj2014 WHERE ico=0";
-$vysledok = mysql_query("$sql");
-if (!$vysledok)
-   {
-
-$sql = "SELECT r101x FROM F$kli_vxcf"."_poznamky_muj2014 WHERE ico=0";
+$sql = "SELECT ico FROM F$kli_vxcf"."_poznamky_muj2014 WHERE ico=0";
 $vysledok = mysql_query("$sql");
 if (!$vysledok)
 {   
@@ -116,8 +111,6 @@ $sqlt = <<<statistika_p1304
 (
    psys         INT DEFAULT 0,
    konx         DECIMAL(10,0) DEFAULT 0,
-   konx8        DECIMAL(10,0) DEFAULT 0,
-   r101x        DECIMAL(10,0) DEFAULT 0,
    ico          DECIMAL(8,0) DEFAULT 0
 );
 statistika_p1304;
@@ -130,69 +123,47 @@ $ttqq = mysql_query("$ttvv");
 
 }
 
-$sql = "SELECT x2fzc FROM F$kli_vxcf"."_poznamky_muj2014 WHERE ico=0";
+$sql = "SELECT ac32 FROM F$kli_vxcf"."_poznamky_muj2014 WHERE ico=0";
 $vysledok = mysql_query("$sql");
 if (!$vysledok)
 {
 echo "def1<br />";
-$sql = "ALTER TABLE F$kli_vxcf"."_poznamky_muj2014 ADD x2fzc DATE NOT NULL AFTER ico";
+$sql = "ALTER TABLE F$kli_vxcf"."_poznamky_muj2014 ADD ac11 DECIMAL(10,2) DEFAULT 0 AFTER konx";
 $vysledek = mysql_query("$sql");
+$sql = "ALTER TABLE F$kli_vxcf"."_poznamky_muj2014 ADD ac12 DECIMAL(10,2) DEFAULT 0 AFTER konx";
+$vysledek = mysql_query("$sql");
+$sql = "ALTER TABLE F$kli_vxcf"."_poznamky_muj2014 ADD ac21 DECIMAL(10,2) DEFAULT 0 AFTER konx";
+$vysledek = mysql_query("$sql");
+$sql = "ALTER TABLE F$kli_vxcf"."_poznamky_muj2014 ADD ac22 DECIMAL(10,2) DEFAULT 0 AFTER konx";
+$vysledek = mysql_query("$sql");
+$sql = "ALTER TABLE F$kli_vxcf"."_poznamky_muj2014 ADD ac31 DECIMAL(10,2) DEFAULT 0 AFTER konx";
+$vysledek = mysql_query("$sql");
+$sql = "ALTER TABLE F$kli_vxcf"."_poznamky_muj2014 ADD ac32 DECIMAL(10,2) DEFAULT 0 AFTER konx";
+$vysledek = mysql_query("$sql");
+
 
 }
 
 //koniec upravy 2014
 
-   }
 //koniec vytvor tabulku v databaze
 
 
 // zapis upravene udaje strana 1
 if ( $copern == 3 AND $strana == 1 )
     {
-$aa1 = strip_tags($_REQUEST['aa1']);
+$ac11 = strip_tags($_REQUEST['ac11']);       
+$ac12 = strip_tags($_REQUEST['ac12']);       
+$ac21 = strip_tags($_REQUEST['ac21']);       
+$ac22 = strip_tags($_REQUEST['ac22']);       
+$ac31 = strip_tags($_REQUEST['ac31']);       
+$ac32 = strip_tags($_REQUEST['ac32']); 
 
 
 $uprav="NO";
 
 $uprtxt = "UPDATE F$kli_vxcf"."_poznamky_muj2014 SET ".
-" aa1='$aa1sql', aa2='$aa2sql', ab1='$ab1',  
-  a1e='$a1e', a2e='$a2e',
-  x1ac='$x1ac', x2ac='$x2ac',
-  x1ad='$x1ad', x2ad='$x2ad', x1ae='$x1ae', x2ae='$x2ae',
-  x1af='$x1af', x2af='$x2af', x1b='$x1b', x2b='$x2b',
-  x1ba='$x1ba', x2ba='$x2ba', x1bb='$x1bb', x2bb='$x2bb',
-  x1c='$x1c', x2c='$x2c',
-  x1e='$x1e', x2e='$x2e', x1ea='$x1ea',
-  x2ea='$x2ea', x1eb='$x1eb', x2eb='$x2eb', x1ed='$x1ed', x2ed='$x2ed', x1ee='$x1ee', x2ee='$x2ee', 
-  x1f='$x1f', x2f='$x2f', x1fa='$x1fa', x2fa='$x2fa',
-  x1fb='$x1fb', x2fb='$x2fb', x1fc='$x1fc', x2fc='$x2fc', x1fd='$x1fd',
-  x2fd='$x2fd', x1fe='$x1fe', x2fe='$x2fe', x1ff='$x1ff', x2ff='$x2ff',
-  x1fg='$x1fg', x2fg='$x2fg', x1fh='$x1fh', x2fh='$x2fh', x1fi='$x1fi',
-  x2fi='$x2fi', x1fjk='$x1fjk', 
-  x2fjk='$x2fjk', x1fjl='$x1fjl', x2fjl='$x2fjl', x1fm='$x1fm',
-  x2fm='$x2fm', x1fn='$x1fn', x2fn='$x2fn', x1fo='$x1fo', x2fo='$x2fo',
-  x1fp='$x1fp', x2fp='$x2fp', x1fq='$x1fq', x2fq='$x2fq', x1fr='$x1fr',
-  x2fr='$x2fr', x1fs='$x1fs', x2fs='$x2fs', x1ftu='$x1ftu', x2ftu='$x2ftu',
-  x1fv='$x1fv', x2fv='$x2fv', x1fw='$x1fw', x2fw='$x2fw', x1fx='$x1fx',
-  x2fx='$x2fx', x1fy='$x1fy', x2fy='$x2fy', 
-  x1g='$x1g', x2g='$x2g', x1ga1='$x1ga1', x2ga1='$x2ga1',
-  x1ga2='$x1ga2', x2ga2='$x2ga2', x1ga3='$x1ga3', x2ga3='$x2ga3',
-  x1ga4='$x1ga4', x2ga4='$x2ga4', x1ga5='$x1ga5', x2ga5='$x2ga5',
-  x1ga6='$x1ga6', x2ga6='$x2ga6', x1gb='$x1gb', x2gb='$x2gb',
-  x1gcd='$x1gcd', x2gcd='$x2gcd', x1ge='$x1ge',
-  x2ge='$x2ge', x1gf='$x1gf', x2gf='$x2gf', x1gg='$x1gg', x2gg='$x2gg',
-  x1gh='$x1gh', x2gh='$x2gh', x1gi='$x1gi', x2gi='$x2gi', x1gj='$x1gj',
-  x2gj='$x2gj', x1gk='$x1gk', x2gk='$x2gk', x1gl='$x1gl', x2gl='$x2gl',
-  x1gm='$x1gm', x2gm='$x2gm', x1h='$x1h', x2h='$x2h', x1ha='$x1ha',
-  x2ha='$x2ha', x1hb='$x1hb', x2hb='$x2hb', x1hcf='$x1hcf',
-  x2hcf='$x2hcf', x1i='$x1i', x2i='$x2i', x1iag='$x1iag', x2iag='$x2iag',
-  x1j='$x1j', x2j='$x2j', x1jae='$x1jae', x2jae='$x2jae',
-  x1jfg='$x1jfg', x2jfg='$x2jfg', x1k='$x1k', x2k='$x2k', x1l='$x1l',
-  x2l='$x2l', x1lab='$x1lab', x2lab='$x2lab', x1lc='$x1lc', x2lc='$x2lc',
-  x1m='$x1m', x2m='$x2m', x1n='$x1n', x2n='$x2n', x1o='$x1o', x2o='$x2o',
-  x1u='$x1u', x2u='$x2u', 
-  x1v='$x1v', x2v='$x2v',
-  x1w='$x1w', x2w='$x2w', ".
+" ac11='$ac11', ac12='$ac12', ac21='$ac21', ac22='$ac22', ac31='$ac31', ac32='$ac32', ".
 " konx8=0 ".
 " WHERE ico >= 0"; 
 
@@ -215,32 +186,12 @@ endif;
 // zapis upravene udaje strana 2
 if ( $copern == 3 AND $strana == 2 )
     {
-$ac11 = strip_tags($_REQUEST['ac11']);       
+     
 
 $uprav="NO";
 
 $uprtxt = "UPDATE F$kli_vxcf"."_poznamky_muj2014 SET ".
-" ac11='$ac11', ac12='$ac12', ac21='$ac21', ac22='$ac22', ac31='$ac31', ac32='$ac32', 
-  ad1='$ad1', ae2='$ae2',
-  af2a='$af2a', af2b='$af2b', af3a='$af3a', af3b='$af3b',
-  af6='$af6sql',
-  ba1='$ba1',
-  bb11a='$bb11a', bb11b='$bb11b',
-  bb11c='$bb11c', bb11d='$bb11d', bb11e='$bb11e', bb12a='$bb12a',
-  bb12b='$bb12b', bb12c='$bb12c', bb12d='$bb12d', bb12e='$bb12e',
-  bb13a='$bb13a', bb13b='$bb13b', bb13c='$bb13c', bb13d='$bb13d',
-  bb13e='$bb13e', bb14a='$bb14a', bb14b='$bb14b', bb14c='$bb14c',
-  bb14d='$bb14d', bb14e='$bb14e', bb15a='$bb15a', bb15b='$bb15b',
-  bb15c='$bb15c', bb15d='$bb15d', bb15e='$bb15e', bb199b='$bb199b',
-  bb199c='$bb199c', bb199d='$bb199d', bb199e='$bb199e',
-  bb21a='$bb21a', bb21b='$bb21bsql', bb21c='$bb21c', bb21d='$bb21d',
-  bb21e='$bb21e', bb21f='$bb21f', bb22a='$bb22a', bb22b='$bb22bsql',
-  bb22c='$bb22c', bb22d='$bb22d', bb22e='$bb22e', bb22f='$bb22f',
-  bb23a='$bb23a', bb23b='$bb23bsql', bb23c='$bb23c', bb23d='$bb23d',
-  bb23e='$bb23e', bb23f='$bb23f', bb299c='$bb299c', bb299d='$bb299d',
-  bb299e='$bb299e', bb299f='$bb299f',
-  ca1='$ca1', cb1='$cb1', cc1='$cc1', cc2='$cc2',
-  cd11='$cd11', cd21='$cd21', cd22='$cd22', cd23='$cd23', ".
+
 " konx8=0 ".        
 " WHERE ico >= 0";  
   
@@ -274,7 +225,7 @@ $uprtxt = "UPDATE F$kli_vxcf"."_poznamky_muj2014 SET ".
 " bb299e=bb21e+bb22e+bb23e,".
 " bb299f=bb21f+bb22f+bb23f ".
 " WHERE ico >= 0 ";
-$upravene = mysql_query("$uprtxt");
+//$upravene = mysql_query("$uprtxt");
 
 
 
@@ -289,9 +240,12 @@ $sqlfir = "SELECT * FROM F$kli_vxcf"."_poznamky_muj2014 WHERE ico >= 0";
 $fir_vysledok = mysql_query($sqlfir);
 $fir_riadok=mysql_fetch_object($fir_vysledok);
 
-$aa1 = $fir_riadok->aa1;
-$aa1sk = SkDatum($aa1);
-$aa2 = $fir_riadok->aa2;
+$ac11 = $fir_riadok->ac11;
+$ac12 = $fir_riadok->ac12;
+$ac21 = $fir_riadok->ac21;
+$ac22 = $fir_riadok->ac22;
+$ac31 = $fir_riadok->ac31;
+$ac32 = $fir_riadok->ac32;
 
 
 mysql_free_result($fir_vysledok);
@@ -320,7 +274,12 @@ var sirkawic = screen.width-10;
 ?>
     function ObnovUI()
     {
-    document.formv1.aa1.value = '<?php echo "$aa1sk";?>';
+    document.formv1.ac11.value = '<?php echo "$ac11";?>';
+    document.formv1.ac12.value = '<?php echo "$ac12";?>';
+    document.formv1.ac21.value = '<?php echo "$ac21";?>';
+    document.formv1.ac22.value = '<?php echo "$ac22";?>';
+    document.formv1.ac31.value = '<?php echo "$ac31";?>';
+    document.formv1.ac32.value = '<?php echo "$ac32";?>';
 
     }
 <?php
@@ -335,7 +294,7 @@ var sirkawic = screen.width-10;
 ?>
     function ObnovUI()
     {
-    document.formv1.ac11.value = '<?php echo "$ac11";?>';
+
 
     }
 <?php
@@ -453,6 +412,7 @@ $clas1="noselect"; $clas2="noselect"; $clas2="noselect";$clas3="noselect"; $clas
 $clas8="noselect"; $clas9="noselect"; $clas10="noselect"; $clas11="noselect"; $clas12="noselect"; $clas13="noselect"; $clas14="noselect";
 if( $strana == 1 ) $clas1="selected";
 if( $strana == 2 ) $clas2="selected";
+if( $strana == 3 ) $clas3="selected";
 if( $strana == 4 ) $clas4="selected";
 if( $strana == 5 ) $clas5="selected";
 if( $strana == 6 ) $clas6="selected";
@@ -477,6 +437,8 @@ if( $strana == 14 ) $clas14="selected";
 
  <a class="<?php echo $clas2; ?>" href="#" onClick="window.open('poznamky_muj2014.php?copern=1&strana=2', '_self');">2</a>
 
+ <a class="<?php echo $clas3; ?>" href="#" onClick="window.open('poznamky_muj2014.php?copern=1&strana=3', '_self');">3</a>
+
 </td>
 <td width="10%" align="center"><INPUT type="submit" id="uloz" name="uloz" value="Uloži zmeny"></td>
 </tr>
@@ -494,12 +456,25 @@ if ( $strana == 1 )
 <td class="rsmall" width="10%"></td><td class="rsmall" width="10%"></td>
 </tr>
 <tr><td class="rmedium" colspan="10"></td></tr>
-<tr><td class="castname" colspan="10">Èl. I Všeobecné údaje</td></tr>
-<tr><td class="medium" colspan="10">Èl. I.2 Údaje o konsolidovanom celku</td></tr>
+<tr><td class="castname" colspan="10">Èl. I Všeobecné údaje.</td></tr>
+<tr><td class="medium" colspan="10">Èl. I.2 Údaje o konsolidovanom celku.</td></tr>
 <tr>
 <td colspan="10" >
 <div class="dtext" >
 <?php $ozntext="A_text1"; $textvypis=vypistextx($ozntext, $mysqlhost, $mysqluser, $mysqlpasswd, $mysqldb, $kli_vxcf); ?>
+<img src="../obr/eshop/note.png" width="25" height="20" onclick="upravtext('<?php echo $ozntext; ?>')" title="Upravi text" >
+<span class="dtextbox">
+<span>&bdquo;</span>&nbsp;<?php echo $textvypis; ?>&nbsp;<span>&rdquo;</span>
+</span>
+</div>
+</td>
+</tr>
+
+<tr><td class="medium" colspan="10">Èl. I.3 Priemerný prepoèítaný poèet zamestnancov.</td></tr>
+<tr>
+<td colspan="10" >
+<div class="dtext" >
+<?php $ozntext="A_text2"; $textvypis=vypistextx($ozntext, $mysqlhost, $mysqluser, $mysqlpasswd, $mysqldb, $kli_vxcf); ?>
 <img src="../obr/eshop/note.png" width="25" height="20" onclick="upravtext('<?php echo $ozntext; ?>')" title="Upravi text" >
 <span class="dtextbox">
 <span>&bdquo;</span>&nbsp;<?php echo $textvypis; ?>&nbsp;<span>&rdquo;</span>
@@ -516,11 +491,165 @@ if ( $strana == 1 )
 
 <?php if ( $strana == 2 ) { ?>
 <tr>
-<td class="rsmall" width="10%"></td><td class="rsmall" width="10%"></td><td class="rsmall" width="10%"></td><td class="rsmall" width="10%"></td>
-<td class="rsmall" width="10%"></td><td class="rsmall" width="10%"></td><td class="rsmall" width="10%"></td><td class="rsmall" width="10%"></td>
+<td class="rsmall" width="10%"></td><td class="rsmall" width="10%"></td><td class="rsmall" width="10%"></td><td class="rsmall" width="5%"></td>
+<td class="rsmall" width="15%"></td><td class="rsmall" width="15%"></td><td class="rsmall" width="5%"></td><td class="rsmall" width="10%"></td>
 <td class="rsmall" width="10%"></td><td class="rsmall" width="10%"></td>
 </tr>
+<tr><td class="rmedium" colspan="10"></td></tr>
+<tr><td class="castname" colspan="10">Èl. II Informácie o prijatých postupoch.</td></tr>
+<tr><td class="medium" colspan="10">Èl. II.1 Nepretržité pokraèovanie v èinnosti.</td></tr>
+<tr>
+<td colspan="10" >
+<div class="dtext" >
+<?php $ozntext="B_text1"; $textvypis=vypistextx($ozntext, $mysqlhost, $mysqluser, $mysqlpasswd, $mysqldb, $kli_vxcf); ?>
+<img src="../obr/eshop/note.png" width="25" height="20" onclick="upravtext('<?php echo $ozntext; ?>')" title="Upravi text" >
+<span class="dtextbox">
+<span>&bdquo;</span>&nbsp;<?php echo $textvypis; ?>&nbsp;<span>&rdquo;</span>
+</span>
+</div>
+</td>
+</tr>
 
+<tr><td class="medium" colspan="10">Èl. II.2 Spôsob oceòovania jednotlivých položiek majetku a záväzkov.</td></tr>
+<tr>
+<td colspan="10" >
+<div class="dtext" >
+<?php $ozntext="B_text2"; $textvypis=vypistextx($ozntext, $mysqlhost, $mysqluser, $mysqlpasswd, $mysqldb, $kli_vxcf); ?>
+<img src="../obr/eshop/note.png" width="25" height="20" onclick="upravtext('<?php echo $ozntext; ?>')" title="Upravi text" >
+<span class="dtextbox">
+<span>&bdquo;</span>&nbsp;<?php echo $textvypis; ?>&nbsp;<span>&rdquo;</span>
+</span>
+</div>
+</td>
+</tr>
+
+<tr><td class="medium" colspan="10">Èl. II.3 Spôsob zostavenia odpisového plánu majetku.</td></tr>
+<tr>
+<td colspan="10" >
+<div class="dtext" >
+<?php $ozntext="B_text3"; $textvypis=vypistextx($ozntext, $mysqlhost, $mysqluser, $mysqlpasswd, $mysqldb, $kli_vxcf); ?>
+<img src="../obr/eshop/note.png" width="25" height="20" onclick="upravtext('<?php echo $ozntext; ?>')" title="Upravi text" >
+<span class="dtextbox">
+<span>&bdquo;</span>&nbsp;<?php echo $textvypis; ?>&nbsp;<span>&rdquo;</span>
+</span>
+</div>
+</td>
+</tr>
+
+<tr><td class="medium" colspan="10">Èl. II.4 Zmeny úètovných zásad a zmeny úètovných metód.</td></tr>
+<tr>
+<td colspan="10" >
+<div class="dtext" >
+<?php $ozntext="B_text4"; $textvypis=vypistextx($ozntext, $mysqlhost, $mysqluser, $mysqlpasswd, $mysqldb, $kli_vxcf); ?>
+<img src="../obr/eshop/note.png" width="25" height="20" onclick="upravtext('<?php echo $ozntext; ?>')" title="Upravi text" >
+<span class="dtextbox">
+<span>&bdquo;</span>&nbsp;<?php echo $textvypis; ?>&nbsp;<span>&rdquo;</span>
+</span>
+</div>
+</td>
+</tr>
+
+<tr><td class="medium" colspan="10">Èl. II.5 Informácie o dotáciách a ich oceòovanie v úètovníctve.</td></tr>
+<tr>
+<td colspan="10" >
+<div class="dtext" >
+<?php $ozntext="B_text5"; $textvypis=vypistextx($ozntext, $mysqlhost, $mysqluser, $mysqlpasswd, $mysqldb, $kli_vxcf); ?>
+<img src="../obr/eshop/note.png" width="25" height="20" onclick="upravtext('<?php echo $ozntext; ?>')" title="Upravi text" >
+<span class="dtextbox">
+<span>&bdquo;</span>&nbsp;<?php echo $textvypis; ?>&nbsp;<span>&rdquo;</span>
+</span>
+</div>
+</td>
+</tr>
+
+<tr><td class="medium" colspan="10">Èl. II.6 Informácie o úètovaní významných opráv chýb minulých úètovných období v bežnom úètovnom období.</td></tr>
+<tr>
+<td colspan="10" >
+<div class="dtext" >
+<?php $ozntext="B_text6"; $textvypis=vypistextx($ozntext, $mysqlhost, $mysqluser, $mysqlpasswd, $mysqldb, $kli_vxcf); ?>
+<img src="../obr/eshop/note.png" width="25" height="20" onclick="upravtext('<?php echo $ozntext; ?>')" title="Upravi text" >
+<span class="dtextbox">
+<span>&bdquo;</span>&nbsp;<?php echo $textvypis; ?>&nbsp;<span>&rdquo;</span>
+</span>
+</div>
+</td>
+</tr>
+
+<?php                     } ?>
+
+
+<?php if ( $strana == 3 ) { ?>
+<tr>
+<td class="rsmall" width="10%"></td><td class="rsmall" width="10%"></td><td class="rsmall" width="10%"></td><td class="rsmall" width="5%"></td>
+<td class="rsmall" width="15%"></td><td class="rsmall" width="15%"></td><td class="rsmall" width="5%"></td><td class="rsmall" width="10%"></td>
+<td class="rsmall" width="10%"></td><td class="rsmall" width="10%"></td>
+</tr>
+<tr><td class="rmedium" colspan="10"></td></tr>
+<tr><td class="castname" colspan="10">Èl. III Informácie, ktoré vysvet¾ujú a dopåòajú súvahu a výkaz ziskov a strát.</td></tr>
+<tr><td class="medium" colspan="10">Èl. III.1 Informácia o sume a dôvodoch vzniku jednotlivých položiek nákladov alebo výnosov, ktoré majú výnimoèný rozsah.</td></tr>
+<tr>
+<td colspan="10" >
+<div class="dtext" >
+<?php $ozntext="C_text1"; $textvypis=vypistextx($ozntext, $mysqlhost, $mysqluser, $mysqlpasswd, $mysqldb, $kli_vxcf); ?>
+<img src="../obr/eshop/note.png" width="25" height="20" onclick="upravtext('<?php echo $ozntext; ?>')" title="Upravi text" >
+<span class="dtextbox">
+<span>&bdquo;</span>&nbsp;<?php echo $textvypis; ?>&nbsp;<span>&rdquo;</span>
+</span>
+</div>
+</td>
+</tr>
+
+<tr><td class="medium" colspan="10">Èl. III.2 Informácie o záväzkoch.</td></tr>
+<tr>
+<td colspan="10" >
+<div class="dtext" >
+<?php $ozntext="C_text2"; $textvypis=vypistextx($ozntext, $mysqlhost, $mysqluser, $mysqlpasswd, $mysqldb, $kli_vxcf); ?>
+<img src="../obr/eshop/note.png" width="25" height="20" onclick="upravtext('<?php echo $ozntext; ?>')" title="Upravi text" >
+<span class="dtextbox">
+<span>&bdquo;</span>&nbsp;<?php echo $textvypis; ?>&nbsp;<span>&rdquo;</span>
+</span>
+</div>
+</td>
+</tr>
+
+<tr><td class="medium" colspan="10">Èl. III.3 Informácie o vlastných akciách.</td></tr>
+<tr>
+<td colspan="10" >
+<div class="dtext" >
+<?php $ozntext="C_text3"; $textvypis=vypistextx($ozntext, $mysqlhost, $mysqluser, $mysqlpasswd, $mysqldb, $kli_vxcf); ?>
+<img src="../obr/eshop/note.png" width="25" height="20" onclick="upravtext('<?php echo $ozntext; ?>')" title="Upravi text" >
+<span class="dtextbox">
+<span>&bdquo;</span>&nbsp;<?php echo $textvypis; ?>&nbsp;<span>&rdquo;</span>
+</span>
+</div>
+</td>
+</tr>
+
+<tr><td class="medium" colspan="10">Èl. III.4 Informácie o orgánoch úètovnej jednotky.</td></tr>
+<tr>
+<td colspan="10" >
+<div class="dtext" >
+<?php $ozntext="C_text4"; $textvypis=vypistextx($ozntext, $mysqlhost, $mysqluser, $mysqlpasswd, $mysqldb, $kli_vxcf); ?>
+<img src="../obr/eshop/note.png" width="25" height="20" onclick="upravtext('<?php echo $ozntext; ?>')" title="Upravi text" >
+<span class="dtextbox">
+<span>&bdquo;</span>&nbsp;<?php echo $textvypis; ?>&nbsp;<span>&rdquo;</span>
+</span>
+</div>
+</td>
+</tr>
+
+<tr><td class="medium" colspan="10">Èl. III.5 Informácie o povinnostiach úètovnej jednotky.</td></tr>
+<tr>
+<td colspan="10" >
+<div class="dtext" >
+<?php $ozntext="C_text5"; $textvypis=vypistextx($ozntext, $mysqlhost, $mysqluser, $mysqlpasswd, $mysqldb, $kli_vxcf); ?>
+<img src="../obr/eshop/note.png" width="25" height="20" onclick="upravtext('<?php echo $ozntext; ?>')" title="Upravi text" >
+<span class="dtextbox">
+<span>&bdquo;</span>&nbsp;<?php echo $textvypis; ?>&nbsp;<span>&rdquo;</span>
+</span>
+</div>
+</td>
+</tr>
 
 <?php                     } ?>
 
