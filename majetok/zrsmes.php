55a347dca1f949dec919b58729f8823f68c398ac
eurosecom <eurosecom1@gmail.com>
2015-02-19 21:30:34 +0100

    MAJ Nulové odpisy zrušenie
    - ak nie je polozka majetku pri spracovani odpisov program ponukne
    riadok na zrusenie

---
+++ majetok/zrsmes.php
@@ -0,0 +1,244 @@
+<HTML>
+<?php
+$sys = 'HIM';
+$urov = 2000;
+$uziv = include("../uziv.php");
+if( !$uziv ) exit;
+
+if(!isset($kli_vxcf)) $kli_vxcf = 1;
+
+       do
+       {
+
+// cislo operacie
+$copern = 1*$_REQUEST['copern'];
+$drupoh = 1*$_REQUEST['drupoh'];
+
+require_once("../pswd/password.php");
+@$spojeni = mysql_connect($mysqlhost, $mysqluser, $mysqlpasswd);
+  if (!$spojeni):
+    echo "Spojenie so serverom nedostupne.";
+    exit;
+  endif;
+  mysql_select_db($mysqldb);
+
+$citfir = include("../cis/citaj_fir.php");
+$mena1 = $fir_mena1;
+$mena2 = $fir_mena2;
+$kurz12 = $fir_kurz12;
+
+
+//zrusenie uzavierky
+if( $copern == 6 )
+           {
+$ume = $_REQUEST['ume'];
+$pole = explode(".", $ume);
+$kli_vmes=$pole[0];
+$kli_vrok=$pole[1];
+
+
+$zmaztt = "DELETE FROM F$kli_vxcf"."_majmajmes WHERE ume=$ume ";
+//echo $zmaztt;
+$zmazane = mysql_query("$zmaztt"); 
+
+$zmaztt = "DELETE FROM F$kli_vxcf"."_majodpisy WHERE ume=$ume ";
+//echo $zmaztt;
+$zmazane = mysql_query("$zmaztt");
+
+if (File_Exists ("../dokumenty/FIR/$kli_vxcf/mesodp.$kli_vmes.pdf")) { $soubor = unlink("../dokumenty/FIR/$kli_vxcf/mesodp.$kli_vmes.pdf"); }
+
+$sqlt = "DROP TABLE F$kli_vxcf"."_majmaj";
+//echo $sqlt;
+
+$vysledok = mysql_query("$sqlt");
+
+$rentt = "RENAME TABLE F$kli_vxcf"."_majmaj_$kli_vmes"."_$kli_vrok"." TO F$kli_vxcf"."_majmaj";
+//echo $rentt;
+
+$vysledok = mysql_query("$rentt");
+
+
+//oznac pohyby hx2=0
+$sqtoz = "UPDATE F$kli_vxcf"."_majpoh".
+" SET hx2=0".
+" WHERE ume = $ume ";
+$oznac = mysql_query("$sqtoz");
+
+$copern=1;
+           }
+
+?> 
+
+<HEAD>
+<META http-equiv="Content-Type" content="text/html; charset=cp1250">
+  <link type="text/css" rel="stylesheet" href="../css/styl.css">
+<title>Zruöenie mesaËnÈho odpisu</title>
+  <style type="text/css">
+
+  </style>
+<script type="text/javascript">
+    
+</script>
+</HEAD>
+<BODY class="white" >
+
+<?php 
+
+
+
+?>
+
+<table class="h2" width="100%" >
+<tr>
+<td>EuroSecom  -  Prehæady a ruöenie mesaËn˝ch odpisov dlhodobÈho majetku</td>
+<td align="right"><span class="login"><?php echo "UME $kli_vume FIR$kli_vxcf-$kli_nxcf  login: $kli_uzmeno $kli_uzprie / $kli_uzid ";?></span></td>
+</tr>
+</table>
+<br />
+
+<?php 
+
+$poslhh = "DROP TABLE F$kli_vxcf"."_majmajmes".$kli_uzid." ";
+$posl = mysql_query("$poslhh");
+$poslhh = "CREATE TABLE F$kli_vxcf"."_majmajmes".$kli_uzid." SELECT * FROM F$kli_vxcf"."_majmajmes ";
+$posl = mysql_query("$poslhh");
+
+//otestuj ci tam je mesiac ak existuje majmaj_1_2015 
+$mesac=1;
+while( $mesac < 13 ) 
+  {
+$poslhh = "SELECT * FROM F$kli_vxcf"."_majmaj_$mesac"."_$kli_vrok ";
+$posl = mysql_query("$poslhh");
+if($posl)
+     {
+//echo $poslhh."<br />";
+
+
+$mesacrok=$mesac.".".$kli_vrok;
+$niejemesac=1;
+$poslhh2 = "SELECT * FROM F$kli_vxcf"."_majmajmes$kli_uzid WHERE ume = $mesacrok ORDER BY ume DESC LIMIT 1";
+$posl2 = mysql_query("$poslhh2"); 
+  if (@$zaznam=mysql_data_seek($posl2,0))
+  {
+  $posled2=mysql_fetch_object($posl2);
+  $niejemesac=0;
+  }
+
+//echo $poslhh2."<br />";
+
+if( $niejemesac == 1 ) 
+       {
+$poslhh3 = "INSERT INTO F$kli_vxcf"."_majmajmes$kli_uzid ( ume, druh, drm, mno ) VALUES ( '$mesacrok', 0, 999, 0 ) ";
+$posl3 = mysql_query("$poslhh3");
+
+       }
+
+//echo $poslhh3."<br />";
+
+     }
+
+
+$mesac=$mesac+1;
+  }
+
+$poslhh = "SELECT * FROM F$kli_vxcf"."_majmajmes$kli_uzid WHERE ume > 0 ORDER BY ume DESC LIMIT 1";
+$posl = mysql_query("$poslhh"); 
+  if (@$zaznam=mysql_data_seek($posl,0))
+  {
+  $posled=mysql_fetch_object($posl);
+  $poslume = $posled->ume;
+  }
+
+$tovtt = "SELECT * FROM F$kli_vxcf"."_majmajmes$kli_uzid ".
+" WHERE ume > 0 ".
+" ORDER BY ume";
+$tov = mysql_query("$tovtt");
+$tvpol = mysql_num_rows($tov);
+if( $tvpol > 0 ) $jetovar=1;
+?>
+
+<?php
+//Ak je tovar tvtvtvtvtvtvtvtvtvtvtvtvtvtvtvtvtvtvtvttvtvtv
+if( $jetovar == 1 )
+           {
+?>
+<table class="vstup" width="100%" >
+<tr>
+<td class="bmenu" width="10%">Obdobie
+<td class="bmenu" width="10%">Poloûiek
+<td class="bmenu" width="10%" align="right" >Obstar·vacia cena
+<td class="bmenu" width="10%" align="right" >Opr·vky
+<td class="bmenu" width="10%" align="right" >Zostatkov· cena
+<td class="bmenu" width="10%" align="right" >MesaËn˝ odpis
+<td class="bmenu" width="10%" align="right" >RoËn˝ odpis
+<td class="bmenu" width="10%" align="right" >RoËn˝ pl·nov.odpis
+<td class="bmenu" width="10%" align="center" >Zostavy
+<td class="bmenu" width="10%" align="center" >Zruöiù spracovanie
+</tr>
+
+<?php
+//zaciatok vypisu
+$i=0;
+  while ($i <= $tvpol )
+  {
+
+  if (@$zaznam=mysql_data_seek($tov,$i))
+{
+$rtov=mysql_fetch_object($tov);
+
+$pole = explode(".", $rtov->ume);
+$kli_vmes=$pole[0];
+$kli_vrok=$pole[1];
+
+?>
+
+<tr>
+<td class="fmenu" >&nbsp;<?php echo $rtov->ume;?></td>
+<td class="fmenu" >&nbsp;<?php echo $rtov->mno;?></td>
+<td class="fmenu" align="right">&nbsp;<?php echo $rtov->cen;?></td>
+<td class="fmenu" align="right">&nbsp;<?php echo $rtov->ops;?></td>
+<td class="fmenu" align="right">&nbsp;<?php echo $rtov->zos;?></td>
+<td class="fmenu" align="right">&nbsp;<?php echo $rtov->mes;?></td>
+<td class="fmenu" align="right">&nbsp;<?php echo $rtov->ros;?></td>
+<td class="fmenu" align="right">&nbsp;<?php echo $rtov->rop;?></td>
+<td class="fmenu" align="center">
+<a href="../dokumenty/FIR<?php echo $kli_vxcf; ?>/mesodp.<?php echo $kli_vmes; ?>.pdf" target="_blank">
+<img src='../obr/tlac.png' width=15 height=10 border=0 alt="Zostava mesaËn˝ch odpisov <?php echo $rtov->ume; ?>" ></a>
+</td>
+<td class="fmenu" align="center">
+<?php
+if( $poslume == $rtov->ume )
+{
+?>
+<a href="zrsmes.php?copern=6&ume=<?php echo $rtov->ume; ?>" target="_self">
+<img src='../obr/zmaz.png' width=15 height=10 border=0 alt="Zruöiù mesaËnÈ odpisy <?php echo $rtov->ume; ?>" ></a>
+<?php
+}
+?>
+</td>
+</tr>
+
+
+<?php
+}
+$i = $i + 1;
+  }
+?>
+
+</table>
+
+<?php
+///////////////////////////////Koniec tlace tovaru tvtvtvtvtvtvtvtvtvtvtvtvtvtvtvtvt
+           }
+?>
+
+
+<?php
+// celkovy koniec dokumentu
+
+$cislista = include("maj_lista.php");
+
+       } while (false);
+?>
+</BODY>
+</HTML>
