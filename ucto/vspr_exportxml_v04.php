07d091512fb2ae20c96928c6807059a53e3305cf
eurosecom <eurosecom1@gmail.com>
2016-02-03 19:27:14 +0100

    Príkazy na úhradu bic(swift) kódy
    #87
    - podľa numsymbolu 5600 PrimaBanka prepne do exportu verzie 04

--- ucto/vspr_exportxml_v04.php
+++ ucto/vspr_exportxml_v04.php
@@ -18,44 +18,6 @@ require_once("../pswd/password.php");
   endif;
   mysql_select_db($mysqldb);
 
-$pain04=0;
-
-$sqlts = "SELECT * FROM F$kli_vxcf"."_uctpriku WHERE dok = $cislo_dok ";
-$sqls = mysql_query("$sqlts");
-$hlavickas=mysql_fetch_object($sqls);
-$uce=1*$hlavickas->uce;
-
-//echo $uce;
-
-$priban=""; 
-$sqlfir = "SELECT * FROM F$kli_vxcf"."_dban WHERE dban = $uce ";
-$fir_vysledok = mysql_query($sqlfir);
-if( $fir_vysledok ) 
-{
-$fir_riadok=mysql_fetch_object($fir_vysledok);
-
-$priban = $fir_riadok->iban;
-}
-
-$xnum = trim(substr($priban,4,4));
-
-//echo $xnum;
-//exit;
-
-if( $xnum == '5600' ) { $pain04=1; }
-
-if( $pain04 == 1 )
-{
-?>
-<script type="text/javascript">
-  var okno = window.open("vspr_exportxml_v04.php?copern=<?php echo $copern; ?>&drupoh=<?php echo $drupoh; ?>
-&page=1&cislo_dok=<?php echo $cislo_dok; ?>&h_uce=<?php echo $uce; ?>","_self");
-</script>
-<?php
-exit;
-}
-
-
 
 //datumove funkcie
 $sDat = include("../funkcie/dat_sk_us.php");
@@ -107,7 +69,7 @@ var sirkawic = screen.width-10;
 <BODY class="white">
  <table class="h2" width="100%" >
  <tr>
-  <td>EuroSecom  -  PrÌkaz na ˙hradu Ë. <?php echo $cislo_dok; ?> - export do XML pain.001.001.03</td>
+  <td>EuroSecom  -  PrÌkaz na ˙hradu Ë. <?php echo $cislo_dok; ?> - export do XML pain.001.001.04</td>
   <td align="right">
    <span class="login"><?php echo "UME $kli_vume FIR$kli_vxcf-$kli_nxcf  login: $kli_uzmeno $kli_uzprie / $kli_uzid ";?></span>
   </td>
@@ -126,30 +88,27 @@ $soubor = fopen("../tmp/$nazsub", "a+");
 //format xml prikazu na uhradu full verzia
 $sqlt = <<<mzdprc
 (
-<Document xmlns="urn:iso:std:iso:20022:tech:xsd:pain.001.001.03" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<?xml version="1.0" encoding="UTF-8"?>
+<Document xmlns="urn:iso:std:iso:20022:tech:xsd:pain.001.001.04" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:iso:std:iso:20022:tech:xsd:pain.001.001.04 pain.001.001.04.xsd">
 <CstmrCdtTrfInitn>
 <GrpHdr>
-<MsgId>ABC/060928/CCT001</MsgId>
-<CreDtTm>2012-09-28T14:07:00</CreDtTm>
-<NbOfTxs>2</NbOfTxs>
-<CtrlSum>21.00</CtrlSum>
+<MsgId>MEDOSPOL.SR.O./160203/154052</MsgId>
+<CreDtTm>2016-02-03T15:40:52</CreDtTm>
+<NbOfTxs>1</NbOfTxs>
+<CtrlSum>18.58</CtrlSum>
 <InitgPty>
-<Nm>INI_PARTY_NAME</Nm>
-<Id>
-<OrgId>
-<Othr>
-<Id>0468651441</Id>
-</Othr>
-</OrgId>
-</Id>
+<Nm>MEDO spol.s r.o.</Nm>
+<PstlAdr>
+<Ctry>SK</Ctry>
+<AdrLine>Cervenej armady 882</AdrLine>
+<AdrLine>Jablonica</AdrLine>
+</PstlAdr>
 </InitgPty>
 </GrpHdr>
 <PmtInf>
-<PmtInfId>ABC/4560/2008-09-25</PmtInfId>
+<PmtInfId>MEDOSPOL.SR.O./160203/154052S00</PmtInfId>
 <PmtMtd>TRF</PmtMtd>
-<BtchBookg>false</BtchBookg>
-<NbOfTxs>1</NbOfTxs>
-<CtrlSum>10.00</CtrlSum>
+<BtchBookg>FALSE</BtchBookg>
 <PmtTpInf>
 <InstrPrty>NORM</InstrPrty>
 <SvcLvl>
@@ -158,330 +117,56 @@ $sqlt = <<<mzdprc
 <LclInstrm>
 <Cd>TRF</Cd>
 </LclInstrm>
-<CtgyPurp>
-<Cd>SALA</Cd>
-</CtgyPurp>
 </PmtTpInf>
-<ReqdExctnDt>2013-03-29</ReqdExctnDt>
+<ReqdExctnDt>2016-02-03</ReqdExctnDt>
 <Dbtr>
-<Nm>MENO PLATITELA MAX 70 CHAR</Nm>
+<Nm>MEDO spol.s r.o.</Nm>
 <PstlAdr>
 <Ctry>SK</Ctry>
-<AdrLine>ADR LINE 01 XXXXXX</AdrLine>
-<AdrLine>ADR LINE 02 XXXXXX</AdrLine>
+<AdrLine>Cervenej armady 882</AdrLine>
+<AdrLine>Jablonica</AdrLine>
 </PstlAdr>
-<Id>
-<OrgId>
-<Othr>
-<Id>0468651441</Id>
-</Othr>
-</OrgId>
-</Id>
 </Dbtr>
 <DbtrAcct>
 <Id>
-<IBAN>SK8902000000000000000123</IBAN>
+<IBAN>SK3956000000003298707001</IBAN>
 </Id>
-<Ccy>EUR</Ccy>
 </DbtrAcct>
 <DbtrAgt>
 <FinInstnId>
-<BIC>SUBASKBX</BIC>
-<Nm>Vseobecna uverova banka</Nm>
+<BICFI>KOMASK2X</BICFI>
 </FinInstnId>
 </DbtrAgt>
-<UltmtDbtr>
-<Nm>ULTIMATE DEBTOR NAME</Nm>
-<PstlAdr>
-<Ctry>SK</Ctry>
-<TwnNm>Malacky</TwnNm>
-<AdrLine>ADR LINE 01 XXXXXX</AdrLine>
-<AdrLine>ADR LINE 02 XXXXXX</AdrLine>
-</PstlAdr>
-<Id>
-<OrgId>
-<Othr>
-<Id>0468651441</Id>
-</Othr>
-</OrgId>
-</Id>
-</UltmtDbtr>
-<ChrgBr>SLEV</ChrgBr>
 <CdtTrfTxInf>
 <PmtId>
-<InstrId> ABC/4562/2008-09-28</InstrId>
-<EndToEndId>/VS1234567890/SS1234567890/KS0308</EndToEndId>
+<InstrId>MEDOSPOL.SR.O./160203/154052/0001</InstrId>
+<EndToEndId>/VS8180/SS70201/KS0558</EndToEndId>
 </PmtId>
-<PmtTpInf>
-<SvcLvl>
-<Cd>SEPA</Cd>
-</SvcLvl>
-</PmtTpInf>
 <Amt>
-<InstdAmt Ccy="EUR">10.00</InstdAmt>
+<InstdAmt Ccy="EUR">18.58</InstdAmt>
 </Amt>
 <CdtrAgt>
 <FinInstnId>
-<BIC>INGBSKBX</BIC>
+<BICFI>SPSRSKBA</BICFI>
 </FinInstnId>
 </CdtrAgt>
 <Cdtr>
-<Nm>SocMetal</Nm>
+<Nm>Rozhlas a televizia Slovenska</Nm>
 <PstlAdr>
 <Ctry>SK</Ctry>
-<AdrLine>U haja 156</AdrLine>
-<AdrLine>2000 DOLNY OHAJ</AdrLine>
+<AdrLine>Mlynska dolina</AdrLine>
+<AdrLine>Bratislava - Lamac</AdrLine>
 </PstlAdr>
-<Id>
-<OrgId>
-<Othr>
-<Id>0468651441</Id>
-</Othr>
-</OrgId>
-</Id>
 </Cdtr>
 <CdtrAcct>
 <Id>
-<IBAN>SK4373000000000000000019</IBAN>
+<IBAN>SK8681800000007000333333</IBAN>
 </Id>
 </CdtrAcct>
-<UltmtCdtr>
-<Nm>ULTIMATE CREDITOR NAME</Nm>
-<PstlAdr>
-<Ctry>SK</Ctry>
-<AdrLine>1. Ulica 158</AdrLine>
-<AdrLine>2000 HORNY OHAJ</AdrLine>
-</PstlAdr>
-<Id>
-<OrgId>
-<Othr>
-<Id>0468651441</Id>
-</Othr>
-</OrgId>
-</Id>
-</UltmtCdtr>
-<Purp>
-<Cd>NORM</Cd>
-</Purp>
-<RegulatoryReportingType1Code>
-<Cd>120</Cd>
-</RegulatoryReportingType1Code>
 <RmtInf>
-<Ustrd>Invoice No/Invoice Date/ Invoice Total Amount/ Invoice Payment amount/ Invoice Remark</Ustrd>
+<Ustrd>Rozhlas a televizia Slovenska</Ustrd>
 </RmtInf>
 </CdtTrfTxInf>
-</PmtInf>
-<PmtInf>
-<PmtInfId>ABC/4560/2008-09-25</PmtInfId>
-<PmtMtd>TRF</PmtMtd>
-<BtchBookg>false</BtchBookg>
-<NbOfTxs>1</NbOfTxs>
-<CtrlSum>11.00</CtrlSum>
-<PmtTpInf>
-<InstrPrty>NORM</InstrPrty>
-<SvcLvl>
-<Cd>SEPA</Cd>
-</SvcLvl>
-<LclInstrm>
-<Cd>TRF</Cd>
-</LclInstrm>
-<CtgyPurp>
-<Cd>SALA</Cd>
-</CtgyPurp>
-</PmtTpInf>
-<ReqdExctnDt>2013-03-07</ReqdExctnDt>
-<Dbtr>
-<Nm>Meno platitela MAX 70 CHAR</Nm>
-<PstlAdr>
-<Ctry>SK</Ctry>
-<AdrLine>ADR LINE 01 XXXXXX</AdrLine>
-<AdrLine>ADR LINE 02 XXXXXX</AdrLine>
-</PstlAdr>
-<Id>
-<OrgId>
-<Othr>
-<Id>0468651441</Id>
-</Othr>
-</OrgId>
-</Id>
-</Dbtr>
-<DbtrAcct>
-<Id>
-<IBAN>SK8902000000000000000123</IBAN>
-</Id>
-<Ccy>EUR</Ccy>
-</DbtrAcct>
-<DbtrAgt>
-<FinInstnId>
-<BIC>SUBASKBX</BIC>
-<Nm>Vseobecna uverova banka</Nm>
-</FinInstnId>
-</DbtrAgt>
-<UltmtDbtr>
-<Nm>ULTIMATE DEBTOR NAME</Nm>
-<PstlAdr>
-<Ctry>SK</Ctry>
-<AdrLine>ADR LINE 01 XXXXXX</AdrLine>
-<AdrLine>ADR LINE 02 XXXXXX</AdrLine>
-</PstlAdr>
-<Id>
-<OrgId>
-<Othr>
-<Id>0468651441</Id>
-</Othr>
-</OrgId>
-</Id>
-</UltmtDbtr>
-<ChrgBr>SLEV</ChrgBr>
-<CdtTrfTxInf>
-<PmtId>
-<InstrId> ABC/4562/2008-1009-28</InstrId>
-<EndToEndId>FIX PAYMENT 123456</EndToEndId>
-</PmtId>
-<PmtTpInf>
-<SvcLvl>
-<Cd>SEPA</Cd>
-</SvcLvl>
-</PmtTpInf>
-<Amt>
-<InstdAmt Ccy="EUR">11.00</InstdAmt>
-</Amt>
-<CdtrAgt>
-<FinInstnId>
-<BIC>INGBSKBX</BIC>
-</FinInstnId>
-</CdtrAgt>
-<Cdtr>
-<Nm>Metal</Nm>
-<PstlAdr>
-<Ctry>SK</Ctry>
-<AdrLine>Hoogstraat 156</AdrLine>
-<AdrLine>2000 DOLNY OHAJ</AdrLine>
-</PstlAdr>
-<Id>
-<OrgId>
-<Othr>
-<Id>0468651441</Id>
-</Othr>
-</OrgId>
-</Id>
-</Cdtr>
-<CdtrAcct>
-<Id>
-<IBAN>SK4373000000000000000019</IBAN>
-</Id>
-</CdtrAcct>
-<UltmtCdtr>
-<Nm>ULTIMATE CREDITOR NAME</Nm>
-<PstlAdr>
-<Ctry>SK</Ctry>
-<AdrLine>Hoogstraat 158</AdrLine>
-<AdrLine>2000 HORNY OHAJ</AdrLine>
-</PstlAdr>
-<Id>
-<OrgId>
-<Othr>
-<Id>0468651441</Id>
-</Othr>
-</OrgId>
-</Id>
-</UltmtCdtr>
-<Purp>
-<Cd>NORM</Cd>
-</Purp>
-<RegulatoryReportingType1Code>
-<Cd>120</Cd>
-</RegulatoryReportingType1Code>
-<RmtInf>
-<Ustrd>Invoice No/Invoice Date/ Invoice Total Amount/ Invoice Payment amount/ Invoice Remark </Ustrd>
-</RmtInf>
-</CdtTrfTxInf>
-</PmtInf>
-</CstmrCdtTrfInitn>
-</Document>
-);
-mzdprc;
-
-//format xml prikazu na uhradu mandatory polozky
-$sqlt = <<<mzdprc
-(
-<Document xmlns="urn:iso:std:iso:20022:tech:xsd:pain.001.001.03" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-<CstmrCdtTrfInitn>
-
-<GrpHdr>
-<MsgId>ABC/060928/CCT001</MsgId>
-<CreDtTm>2012-09-28T14:07:00</CreDtTm>
-<NbOfTxs>2</NbOfTxs>
-</GrpHdr>
-
-
-
-<PmtInf>
-
-<PmtInfId>ABC/4560/2008-09-25</PmtInfId>
-<PmtMtd>TRF</PmtMtd>
-<ReqdExctnDt>2013-03-29</ReqdExctnDt>
-
-<Dbtr>
-<Nm>MENO PLATITELA MAX 70 CHAR</Nm>
-</Dbtr>
-
-<DbtrAcct>
-<Id>
-<IBAN>SK8902000000000000000123</IBAN>
-</Id>
-<Ccy>EUR</Ccy>
-</DbtrAcct>
-
-<DbtrAgt>
-<FinInstnId>
-<BIC>SUBASKBX</BIC>
-<Nm>Vseobecna uverova banka</Nm>
-</FinInstnId>
-</DbtrAgt>
-
-<ChrgBr>SLEV</ChrgBr>
-
-
-//jedna polozka platby
-
-<CdtTrfTxInf>
-
-<PmtId>
-<InstrId> ABC/4562/2008-09-28</InstrId>
-<EndToEndId>/VS1234567890/SS1234567890/KS0308</EndToEndId>
-</PmtId>
-
-<Amt>
-<InstdAmt Ccy="EUR">10.00</InstdAmt>
-</Amt>
-
-<CdtrAgt>
-<FinInstnId>
-<BIC>INGBSKBX</BIC>
-</FinInstnId>
-</CdtrAgt>
-
-<Cdtr>
-<Nm>SocMetal</Nm>
-<PstlAdr>
-<Ctry>SK</Ctry>
-<AdrLine>U haja 156</AdrLine>
-<AdrLine>2000 DOLNY OHAJ</AdrLine>
-</PstlAdr>
-</Cdtr>
-
-<CdtrAcct>
-<Id>
-<IBAN>SK4373000000000000000019</IBAN>
-</Id>
-</CdtrAcct>
-
-</CdtTrfTxInf>
-
-//koniec jedna polozka platby
-
-
 </PmtInf>
 </CstmrCdtTrfInitn>
 </Document>
@@ -526,7 +211,8 @@ if ( $j == 0 )
      {
 
   $text = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"."\r\n"; fwrite($soubor, $text);
-  $text = "<Document xmlns=\"urn:iso:std:iso:20022:tech:xsd:pain.001.001.03\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">"."\r\n"; fwrite($soubor, $text);		
+  $text = "<Document xmlns=\"urn:iso:std:iso:20022:tech:xsd:pain.001.001.04\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" ".		
+          " xsi:schemaLocation=\"urn:iso:std:iso:20022:tech:xsd:pain.001.001.04 pain.001.001.04.xsd\">"."\r\n"; fwrite($soubor, $text);
   $text = " <CstmrCdtTrfInitn>"."\r\n"; fwrite($soubor, $text);
 
   $text = " <GrpHdr>"."\r\n"; fwrite($soubor, $text);
@@ -596,12 +282,6 @@ $hicoad2 = StrTr($hicoad2, "·‰ËÔÈÏÎÌæÚÙÛˆ‡¯öù˙˘¸˝û¡ƒ»œ…ÃÀÕº“”÷‘ÿ¿äç⁄Ÿ‹›é",
   $text = "  <AdrLine>".$hicoad2."</AdrLine>"."\r\n"; fwrite($soubor, $text);
   $text = "  </PstlAdr>"."\r\n"; fwrite($soubor, $text);
 
-  $text = "  <Id>"."\r\n"; fwrite($soubor, $text);
-  $text = "  <OrgId>"."\r\n"; fwrite($soubor, $text);
-  $text = "  <BICOrBEI>".$prbic."</BICOrBEI>"."\r\n"; fwrite($soubor, $text);
-  $text = "  </OrgId>"."\r\n"; fwrite($soubor, $text);
-  $text = "  </Id>"."\r\n"; fwrite($soubor, $text);
-
 
   $text = "  </InitgPty>"."\r\n"; fwrite($soubor, $text);
 
@@ -614,21 +294,19 @@ $hicoad2 = StrTr($hicoad2, "·‰ËÔÈÏÎÌæÚÙÛˆ‡¯öù˙˘¸˝û¡ƒ»œ…ÃÀÕº“”÷‘ÿ¿äç⁄Ÿ‹›é",
   $text = "  <PmtInfId>BAN".$bankauhrady."/DOK".$hlavicka->dok."/".$datumuhrady."</PmtInfId>"."\r\n"; fwrite($soubor, $text);
   $text = "  <PmtMtd>TRF</PmtMtd>"."\r\n"; fwrite($soubor, $text);
 
-  $text = "  <BtchBookg>true</BtchBookg>"."\r\n"; fwrite($soubor, $text);
-
-  $text = "  <NbOfTxs>".$pocpol."</NbOfTxs>"."\r\n"; fwrite($soubor, $text);
-
-  $text = "  <CtrlSum>".$celksuma."</CtrlSum>"."\r\n"; fwrite($soubor, $text);
+  $text = "  <BtchBookg>FALSE</BtchBookg>"."\r\n"; fwrite($soubor, $text);
 
   $text = " <PmtTpInf>"."\r\n"; fwrite($soubor, $text);
+
+  $text = "  <InstrPrty>NORM</InstrPrty>"."\r\n"; fwrite($soubor, $text);
 
   $text = " <SvcLvl>"."\r\n"; fwrite($soubor, $text);
   $text = "  <Cd>SEPA</Cd>"."\r\n"; fwrite($soubor, $text);
   $text = " </SvcLvl>"."\r\n"; fwrite($soubor, $text);
 
-  $text = " <CtgyPurp>"."\r\n"; fwrite($soubor, $text);
-  $text = "  <Cd>CASH</Cd>"."\r\n"; fwrite($soubor, $text);
-  $text = " </CtgyPurp>"."\r\n"; fwrite($soubor, $text);
+  $text = " <LclInstrm>"."\r\n"; fwrite($soubor, $text);
+  $text = "  <Cd>TRF</Cd>"."\r\n"; fwrite($soubor, $text);
+  $text = " </LclInstrm>"."\r\n"; fwrite($soubor, $text);
 
   $text = " </PmtTpInf>"."\r\n"; fwrite($soubor, $text);
 
@@ -641,12 +319,6 @@ $hicoad2 = StrTr($hicoad2, "·‰ËÔÈÏÎÌæÚÙÛˆ‡¯öù˙˘¸˝û¡ƒ»œ…ÃÀÕº“”÷‘ÿ¿äç⁄Ÿ‹›é",
   $text = "  <AdrLine>".$hicoad1."</AdrLine>"."\r\n"; fwrite($soubor, $text);
   $text = "  <AdrLine>".$hicoad2."</AdrLine>"."\r\n"; fwrite($soubor, $text);
   $text = "  </PstlAdr>"."\r\n"; fwrite($soubor, $text);
-
-  $text = "  <Id>"."\r\n"; fwrite($soubor, $text);
-  $text = "  <OrgId>"."\r\n"; fwrite($soubor, $text);
-  $text = "  <BICOrBEI>".$prbic."</BICOrBEI>"."\r\n"; fwrite($soubor, $text);
-  $text = "  </OrgId>"."\r\n"; fwrite($soubor, $text);
-  $text = "  </Id>"."\r\n"; fwrite($soubor, $text);
   $text = " </Dbtr>"."\r\n"; fwrite($soubor, $text);
 
   $text = " <DbtrAcct>"."\r\n"; fwrite($soubor, $text);
@@ -659,16 +331,13 @@ $hicoad2 = StrTr($hicoad2, "·‰ËÔÈÏÎÌæÚÙÛˆ‡¯öù˙˘¸˝û¡ƒ»œ…ÃÀÕº“”÷‘ÿ¿äç⁄Ÿ‹›é",
 
   $text = " <DbtrAgt>"."\r\n"; fwrite($soubor, $text);
   $text = " <FinInstnId>"."\r\n"; fwrite($soubor, $text);
-  $text = "  <BIC>".$prbic."</BIC>"."\r\n"; fwrite($soubor, $text);
+  $text = "  <BICFI>".$prbic."</BICFI>"."\r\n"; fwrite($soubor, $text);
 
 $bname = iconv("CP1250", "UTF-8", $nban);
 
   $text = "  <Nm>".$bname."</Nm>"."\r\n"; fwrite($soubor, $text);
   $text = " </FinInstnId>"."\r\n"; fwrite($soubor, $text);
   $text = " </DbtrAgt>"."\r\n"; fwrite($soubor, $text);
-
-
-  $text = " <ChrgBr>SLEV</ChrgBr>"."\r\n"; fwrite($soubor, $text);
 
 
      }
@@ -724,7 +393,7 @@ $instdamt=$hlavickav->hodp;
 
   $text = " <CdtrAgt>"."\r\n"; fwrite($soubor, $text);
   $text = " <FinInstnId>"."\r\n"; fwrite($soubor, $text);
-  $text = "  <BIC>".$hlavickav->pbic."</BIC>"."\r\n"; fwrite($soubor, $text);
+  $text = "  <BICFI>".$hlavickav->pbic."</BICFI>"."\r\n"; fwrite($soubor, $text);
   $text = " </FinInstnId>"."\r\n"; fwrite($soubor, $text);
   $text = " </CdtrAgt>"."\r\n"; fwrite($soubor, $text);
 
@@ -818,13 +487,6 @@ if( $pstat == '' ) { $pstat="SK"; }
   $text = "  <AdrLine>".$icoad1."</AdrLine>"."\r\n"; fwrite($soubor, $text);
   $text = "  <AdrLine>".$icoad2."</AdrLine>"."\r\n"; fwrite($soubor, $text);
   $text = " </PstlAdr>"."\r\n"; fwrite($soubor, $text);
-
-  $text = "  <Id>"."\r\n"; fwrite($soubor, $text);
-  $text = "  <OrgId>"."\r\n"; fwrite($soubor, $text);
-  $text = "  <BICOrBEI>".$hlavickav->pbic."</BICOrBEI>"."\r\n"; fwrite($soubor, $text);
-  $text = "  </OrgId>"."\r\n"; fwrite($soubor, $text);
-  $text = "  </Id>"."\r\n"; fwrite($soubor, $text);
-
   $text = " </Cdtr>"."\r\n"; fwrite($soubor, $text);
 
   $text = " <CdtrAcct>"."\r\n"; fwrite($soubor, $text);
@@ -836,15 +498,6 @@ $ibanxx=strtoupper($ibanxx);
   $text = "  <IBAN>".$ibanxx."</IBAN>"."\r\n"; fwrite($soubor, $text);
   $text = "  </Id>"."\r\n"; fwrite($soubor, $text);
   $text = " </CdtrAcct>"."\r\n"; fwrite($soubor, $text);
-
-  $text = " <InstrForCdtrAgt>"."\r\n"; fwrite($soubor, $text);
-  $text = "  <InstrInf>Instrukcia uhrady</InstrInf>"."\r\n"; fwrite($soubor, $text);
-  $text = " </InstrForCdtrAgt>"."\r\n"; fwrite($soubor, $text);
-
-
-  $text = " <Purp>"."\r\n"; fwrite($soubor, $text);
-  $text = "  <Cd>OTHR</Cd>"."\r\n"; fwrite($soubor, $text);
-  $text = " </Purp>"."\r\n"; fwrite($soubor, $text);
 
   $text = " <RmtInf>"."\r\n"; fwrite($soubor, $text);
   $text = "  <Ustrd>Informacia pre prijemcu</Ustrd>"."\r\n"; fwrite($soubor, $text);
