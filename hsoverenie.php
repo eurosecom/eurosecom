078bf8901b8329f329fc3b2a69aae706bc9bba2a
eurosecom <eurosecom1@gmail.com>
2016-07-14 10:15:22 +0200

    Drobné úpravy
    - Verzia 2016_07

--- hsoverenie.php
+++ hsoverenie.php
@@ -207,7 +207,7 @@ if( $cook == 1 )
     $_SESSION['kli_xuzfak'] = $riadok->fak_prav;
     $_SESSION['kli_xuzvyr'] = $riadok->vyr_prav;
     $_SESSION['kli_txt1'] = $riadok->txt1;
-    $_SESSION['verzia'] = '2016_02';
+    $_SESSION['verzia'] = '2016_07';
 
 
     $ipad=$_SERVER["REMOTE_ADDR"];
