46012ede78dc9c9a5ef2377d040025bf7497f2b5
eurosecom <eurosecom1@gmail.com>
2017-01-11 17:50:45 +0100

    Oznámenie o úprave ZD
    #158
    - všetky položky vytvorené v tabulke
    
    TODO
    - schovať prepínanie strán keď som v zozname
    - poziciovať inputy na strane 1 a 2, nechať aj input ico

--- ucto/oznamenie_uprzd.php
+++ ucto/oznamenie_uprzd.php
@@ -38,11 +38,7 @@ $strana = 1*$_REQUEST['strana'];
 if ( $strana == 0 ) $strana=1;
 $zoznamaut=1;
 
-$vsetkyprepocty=0;
-$prepocitaj = 1*$_REQUEST['prepocitaj'];
-
-$pocetdni = 1*$_REQUEST['pocetdni'];
-$vypocitajdan = 1*$_REQUEST['vypocitajdan'];
+$cislo_cpl = 1*$_REQUEST['cislo_cpl'];
 
 $xml = 1*$_REQUEST['xml'];
 
@@ -56,12 +52,12 @@ $rmc1=0;
     {
 $cislo_cpl = 1*$_REQUEST['cislo_cpl'];
 $copern=20;
-$strana=3;
+$strana=1;
 $zoznamaut=0;
     }
 //koniec uprav vozidlo
 
-//nove vozidlo
+//nove 
     if ( $copern == 336 )
     {
 $sql = "INSERT INTO F".$kli_vxcf."_uctoznamenie_uprzd (oc,konx1) VALUES ( 1, 0 ) ";
@@ -76,21 +72,21 @@ $sqldok = mysql_query("$sqlttt");
  $cislo_cpl=$riaddok->cpl;
  }
 $copern=20;
-$strana=3;
+$strana=1;
 $zoznamaut=0;
 $_REQUEST['cislo_cpl']=$cislo_cpl;
     }
-//koniec nove vozidlo
+//koniec nove 
 
-//zmaz vozidlo
+//zmaz 
     if ( $copern == 316 )
     {
 $cislo_cpl = 1*$_REQUEST['cislo_cpl'];
-$cislo_spz = $_REQUEST['cislo_spz'];
+$cislo_ico = 1*$_REQUEST['cislo_ico'];
 ?>
 <script type="text/javascript">
-if( !confirm ("Chcete zmazaù vozidlo <?php echo $cislo_spz; ?> ?") )
-         { location.href='oznamenie_uprzd.php?cislo_oc=9999&drupoh=1&page=1&subor=0&copern=20&strana=5' }
+if( !confirm ("Chcete zmazaù iËo <?php echo $cislo_ico; ?> ?") )
+         { location.href='oznamenie_uprzd.php?cislo_oc=9999&drupoh=1&page=1&subor=0&copern=20&strana=5&cislo_cpl=<?php echo $cislo_cpl; ?>' }
 else
          { location.href='oznamenie_uprzd.php?copern=3166&page=1&drupoh=1&cislo_cpl=<?php echo $cislo_cpl; ?>' }
 </script>
@@ -112,39 +108,31 @@ $zoznamaut=1;
 //zmaz vozidlo
 
 
-
 //zapis upravene udaje
 if ( $copern == 23 )
      {
+
+$cislo_cpl = 1*$_REQUEST['cislo_cpl'];
+
+
 if ( $strana == 1 ) {
 
-$zoema = strip_tags($_REQUEST['zoema']);
+$ico = strip_tags($_REQUEST['ico']);
 
 
 $uprtxt = "UPDATE F$kli_vxcf"."_uctoznamenie_uprzd SET ".
-" zoema='$zoema' ".
-" WHERE oc = 9999 ";
+" ico='$ico' ".
+" WHERE oc = 1 AND cpl = $cislo_cpl ";
                     }
 
 if ( $strana == 2 ) {
 
-$rdc3 = strip_tags($_REQUEST['rdc3']);
+$suma = strip_tags($_REQUEST['suma']);
 
 
 $uprtxt = "UPDATE F$kli_vxcf"."_uctoznamenie_uprzd SET ".
-" rdc3='$rdc3' ".
-" WHERE oc = 9999 ";
-
-                    }
-
-if ( $strana == 3 ) {
-
-$rdc3 = strip_tags($_REQUEST['rdc3']);
-
-
-$uprtxt = "UPDATE F$kli_vxcf"."_uctoznamenie_uprzd SET ".
-" rdc3='$rdc3' ".
-" WHERE oc = 9999 ";
+" suma='$suma' ".
+" WHERE oc = 1 AND cpl = $cislo_cpl ";
 
                     }
 
@@ -152,14 +140,6 @@ $uprtxt = "UPDATE F$kli_vxcf"."_uctoznamenie_uprzd SET ".
 $upravene = mysql_query("$uprtxt");
 
 $copern=20;
-if (!$upravene):
-?>
-<script type="text/javascript"> alert( "⁄DAJE NEBOLI UPRAVEN…" ) </script>
-<?php
-endif;
-if ($upravene):
-$uprav="OK";
-endif;
      }
 //koniec zapisu upravenych udajov
 
@@ -167,7 +147,7 @@ endif;
 
 
 //vytvorenie
-$sql = "SELECT konx3 FROM F".$kli_vxcf."_uctoznamenie_uprzd";
+$sql = "SELECT iu30a FROM F".$kli_vxcf."_uctoznamenie_uprzd";
 $vysledok = mysql_query($sql);
 if (!$vysledok)
 {
@@ -184,6 +164,19 @@ $sqlt = <<<mzdprc
    datum         DATE NOT NULL,
 
    ico           DECIMAL(10,0) DEFAULT 0,
+   sdic          VARCHAR(20) NOT NULL,
+   sprie         VARCHAR(40) NOT NULL,
+   smeno         VARCHAR(40) NOT NULL,
+   stitl         VARCHAR(10) NOT NULL,
+   stitz         VARCHAR(10) NOT NULL,
+   snai          VARCHAR(50) NOT NULL,
+   suli          VARCHAR(10) NOT NULL,
+   scdm          VARCHAR(10) NOT NULL,
+   smes          VARCHAR(50) NOT NULL,
+   sstat         VARCHAR(10) NOT NULL,
+
+   iu30a         DECIMAL(1,0) DEFAULT 0,
+   iu30b         DECIMAL(1,0) DEFAULT 0,
    suma          DECIMAL(10,2) DEFAULT 0,
 
    konx          DECIMAL(10,0) DEFAULT 0,
@@ -201,11 +194,13 @@ $sql = "INSERT INTO F".$kli_vxcf."_uctoznamenie_uprzd (oc,konx1) VALUES ( 9999, 
 $vysledok = mysql_query($sql);
 }
 
-$sql = "SELECT rxx FROM F".$kli_vxcf."_uctoznamenie_uprzd";
+$sql = "SELECT vypr FROM F".$kli_vxcf."_uctoznamenie_uprzd";
 $vysledok = mysql_query($sql);
 if (!$vysledok)
 {
-$sql = "ALTER TABLE F$kli_vxcf"."_uctoznamenie_uprzd ADD rxx DECIMAL(10,2) DEFAULT 0 AFTER konx";
+$sql = "ALTER TABLE F$kli_vxcf"."_uctoznamenie_uprzd ADD vtel VARCHAR(40) NOT NULL AFTER konx";
+$vysledek = mysql_query("$sql");
+$sql = "ALTER TABLE F$kli_vxcf"."_uctoznamenie_uprzd ADD vypr VARCHAR(40) NOT NULL AFTER konx";
 $vysledek = mysql_query("$sql");
 }
 
@@ -217,26 +212,21 @@ $vysledek = mysql_query("$sql");
 if ( $copern == 20 )
      {
 $sqlfir = "SELECT * FROM F$kli_vxcf"."_uctoznamenie_uprzd".
-" WHERE oc = 9999 ORDER BY oc";
+" WHERE oc = 1 AND cpl = $cislo_cpl ";
 $fir_vysledok = mysql_query($sqlfir);
 $fir_riadok=mysql_fetch_object($fir_vysledok);
 
 if ( $strana == 1 ) {
 
-$zoema = $fir_riadok->druh;
+$ico = $fir_riadok->ico;
                     }
 
 if ( $strana == 2 ) {
 
-$rdc3 = $fir_riadok->rdc3;
+$suma = $fir_riadok->suma;
 
                     }
 
-if ( $strana == 3 ) {
-
-$rdc3 = $fir_riadok->rdc3;
-
-                    }
 
 mysql_free_result($fir_vysledok);
      }
@@ -278,7 +268,7 @@ $fir_fnaz = "";
 <META http-equiv="Content-Type" content="text/html; charset=cp1250">
  <link rel="stylesheet" href="../css/reset.css">
  <link rel="stylesheet" href="../css/tlaciva.css">
-<title>EuroSecom - DaÚ z motorov˝ch vozidiel</title>
+<title>EuroSecom - Ozn·menie o ˙prave ZD</title>
 <style type="text/css">
 div.sadzby-area {
   position: absolute;
@@ -450,20 +440,15 @@ div.input-echo {
   {
 <?php if ( $strana == 1 OR $strana == 9999 ) { ?>
 
-   document.formv1.zoema.value = '<?php echo "$zoema";?>';
+   document.formv1.ico.value = '<?php echo "$ico";?>';
 <?php                                        } ?>
 
 <?php if ( $strana == 2 OR $strana == 9999 ) { ?>
 
-   document.formv1.rdc3.value = '<?php echo "$rdc3";?>';
+   document.formv1.suma.value = '<?php echo "$suma";?>';
 
 <?php                                        } ?>
 
-<?php if ( $strana == 3 OR $strana == 9999 ) { ?>
-
-   document.formv1.rdc3.value = '<?php echo "$rdc3";?>';
-
-<?php                                        } ?>
 
    }
 <?php
@@ -485,37 +470,23 @@ div.input-echo {
 
   function TlacDMV()
   {
-   window.open('../ucto/oznamenie_uprzd.php?cislo_oc=<?php echo $cislo_oc;?>&copern=10&drupoh=1&page=1&subor=0&strana=9999', '_blank', 'width=1050, height=900, top=0, left=20, status=yes, resizable=yes, scrollbars=yes');
+   window.open('../ucto/oznamenie_uprzd.php?cislo_oc=<?php echo $cislo_oc;?>&copern=10&drupoh=1&page=1&subor=0&strana=9999&cislo_cpl=<?php echo $cislo_cpl; ?>', '_blank', 'width=1050, height=900, top=0, left=20, status=yes, resizable=yes, scrollbars=yes');
   }
-  function NacitajMinRok()
-  {
-   window.open('../ucto/oznamenie_uprzd.php?cislo_oc=<?php echo $cislo_oc;?>&copern=3155&drupoh=1&page=1', '_self', 'width=1060, height=900, top=0, left=12, status=yes, resizable=yes, scrollbars=yes')
-  }
+
   function PoucVyplnenie()
   {
    window.open('../dokumenty/dan_z_prijmov2015/dpdmv2015/dmv_v15_poucenie.pdf', '_blank', 'width=1080, height=900, top=0, left=20, status=yes, resizable=yes, scrollbars=yes');
   }
-  function Sadzby2015()
-  {
-   window.open('../dokumenty/dan_z_prijmov2015/dpdmv2015/dmv_v15_sadzby.pdf', '_blank', 'width=1080, height=900, top=0, left=20, status=yes, resizable=yes, scrollbars=yes');
-  }
 
-
-  function VytvorOznamZanik(cpl)
-  {
-   var cislo_cpl = cpl;
-   window.open('../ucto/oznamenie_uprzd.php?cislo_oc=<?php echo $cislo_oc;?>&copern=70&drupoh=1&page=1&cislo_cpl='+ cislo_cpl + '&ukoncenie=1',
- '_blank','width=1060, height=900, top=0, left=12, status=yes, resizable=yes, scrollbars=yes' )
-  }
   function UpravVzd(cpl)
   {
    var cislo_cpl = cpl;
    window.open('../ucto/oznamenie_uprzd.php?copern=346&cislo_cpl='+ cislo_cpl + '&uprav=0', '_self' )
   }
-  function ZmazVzd(cpl, cislo_spz)
+  function ZmazVzd(cpl, cislo_ico)
   {
    var cislo_cpl = cpl;
-   window.open('../ucto/oznamenie_uprzd.php?copern=316&cislo_cpl='+ cislo_cpl + '&cislo_spz='+ cislo_spz + '&uprav=0', '_self' )
+   window.open('../ucto/oznamenie_uprzd.php?copern=316&cislo_cpl='+ cislo_cpl + '&cislo_ico='+ cislo_ico + '&uprav=0', '_self' )
   }
   function NoveVzd()
   {
@@ -523,19 +494,9 @@ div.input-echo {
   }
   function DMVdoXML()
   {
-   window.open('../ucto/oznamenie_uprzd.php?copern=110&page=1&sysx=UCT&drupoh=1&uprav=1',
+   window.open('../ucto/oznamenie_uprzd.php?copern=110&page=1&sysx=UCT&drupoh=1&uprav=1&cislo_cpl=<?php echo $cislo_cpl; ?>',
  '_blank', 'width=1080, height=900, top=0, left=10, status=yes, resizable=yes, scrollbars=yes');
   }
-
-<?php if ( $copern == 20 ) { ?>
-  function VyberZhasni(sadzba)
-  {
-   document.formv1.r12.value=sadzba;
-   document.getElementById('sadzby').className='hidden';
-   document.formv1.r12.focus();
-   document.formv1.r12.select();
-  }
-<?php                      } ?>
 </script>
 </HEAD>
 <BODY id="white" onload="ObnovUI();">
@@ -566,13 +527,13 @@ div.input-echo {
 </div>
 
 <div id="content">
-<FORM name="formv1" method="post" action="oznamenie_uprzd.php?copern=23&cislo_oc=<?php echo $cislo_oc;?>&cislo_cpl=<?php echo $cislo_cpl;?>&strana=<?php echo $strana;?>">
+<FORM name="formv1" method="post" action="oznamenie_uprzd.php?copern=23&cislo_oc=1&cislo_cpl=<?php echo $cislo_cpl;?>&strana=<?php echo $strana;?>">
 <?php
 $clas1="noactive"; $clas2="noactive"; $clas3="noactive"; $clas4="noactive"; $clas5="noactive";
 if ( $strana == 1 ) $clas1="active"; if ( $strana == 2 ) $clas2="active"; if ( $strana == 3 ) $clas3="active";
 if ( $strana == 4 ) $clas4="active"; if ( $strana == 5 ) $clas5="active";
 
-$source="../ucto/oznamenie_uprzd.php?cislo_oc=".$cislo_oc."&drupoh=1&page=1&subor=0";
+$source="../ucto/oznamenie_uprzd.php?cislo_oc=1&drupoh=1&page=1&subor=0&cislo_cpl=$cislo_cpl";
 ?>
 <div class="navbar">
  <a href="#" onclick="window.open('<?php echo $source; ?>&copern=20&strana=1', '_self');" class="<?php echo $clas1; ?> toleft">1</a>
@@ -590,6 +551,7 @@ $source="../ucto/oznamenie_uprzd.php?cislo_oc=".$cislo_oc."&drupoh=1&page=1&subo
 
 
 <div class="input-echo" style="width:635px; top:100px; left:52px;"><?php echo $duli; ?></div>
+<input type="text" name="ico" id="ico" style="width:359px; top:100px; left:51px;"/>
 
 <?php                                        } ?>
 
@@ -600,20 +562,11 @@ $source="../ucto/oznamenie_uprzd.php?cislo_oc=".$cislo_oc."&drupoh=1&page=1&subo
 <span class="text-echo" style="top:75px; left:406px;"><?php echo $fir_fdic;?></span>
 
 
-<input type="text" name="d3prie" id="d3prie" style="width:359px; top:100px; left:51px;"/>
+<input type="text" name="suma" id="suma" style="width:359px; top:100px; left:51px;"/>
 
 <?php                                        } ?>
 
 
-<?php if ( $strana == 3 OR $strana == 9999 ) { ?>
-<img src="../dokumenty/dan_z_prijmov2016/ozn176v16/ozn176_v16_str1.jpg"
-     alt="tlaËivo DaÚ z motorov˝ch vozidiel pre rok 2015 2.strana 380kB" class="form-background">
-<span class="text-echo" style="top:75px; left:406px;"><?php echo $fir_fdic;?></span>
-
-
-<input type="text" name="d3prie" id="d3prie" style="width:359px; top:100px; left:51px;"/>
-
-<?php                                        } ?>
 
 
 <?php if ( $strana == 5 OR $strana == 9999 ) {
@@ -648,19 +601,20 @@ $i=0;
  {
 $rsluz=mysql_fetch_object($sluz);
 $cisloi=$i+1;
+
 ?>
 <tr class="body"> 
  <td align="left"><?php echo $cisloi.". ".$rsluz->ico; ?></td>
- <td><?php echo $dic; ?></td>
- <td align="center"><?php echo $nai; ?></td>
- <td align="center"><?php echo $uli; ?></td>
- <td align="center"><?php echo $mes; ?></td>
- <td align="right" style="">x<?php echo $rsluz->suma; ?></td>
+ <td><?php echo $rsluz->sdic; ?></td>
+ <td align="center"><?php echo $rsluz->snai; ?></td>
+ <td align="center"><?php echo $rsluz->suli; ?></td>
+ <td align="center"><?php echo $rsluz->smes; ?></td>
+ <td align="right" style=""><?php echo $rsluz->suma; ?></td>
 <td align="center">
   <img src="../obr/ikony/pencil_blue_icon.png" onclick="UpravVzd(<?php echo $rsluz->cpl; ?>);"
-       title="Upraviù">&nbsp;&nbsp;&nbsp;
-  <img src="../obr/ikony/xmark_lred_icon.png" onclick="ZmazVzd(<?php echo $rsluz->cpl; ?>, '<?php echo $rsluz->vzspz; ?>');"
-       title="Vymazaù">
+       title="Upraviù <?php echo $rsluz->cpl; ?>">&nbsp;&nbsp;&nbsp;
+  <img src="../obr/ikony/xmark_lred_icon.png" onclick="ZmazVzd(<?php echo $rsluz->cpl; ?>, '<?php echo $rsluz->ico; ?>');"
+       title="Vymazaù <?php echo $rsluz->cpl; ?>">
  </td>
 </tr>
 <?php
