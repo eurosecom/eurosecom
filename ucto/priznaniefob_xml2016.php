47afe54eb7a2020dba73d8556f7c4310006fb4fe
eurosecom <eurosecom1@gmail.com>
2017-03-01 12:27:44 +0100

    DPFOBv16
    #153
    - zlúčené upozornenie s xml z 9.1.2017
    - oprava r91, R100 a R104 chce nulu nie prázdne
    - oprava r134, r135 a r136
    - načítané na FS portál
    
    TODO
    - skúsiť načítat reálne dáta z nejakej firmy
    -
    - načítané

--- ucto/priznaniefob_xml2016.php
+++ ucto/priznaniefob_xml2016.php
@@ -196,6 +196,7 @@ $hlavicka=mysql_fetch_object($sql);
 
 $obdobie=$kli_vmes;
 $dat_dat = Date ("d.m.Y", MkTime (date("H"),date("i"),date("s"),date("m"),date("d"),date("Y")));
+$dat_datsql = Date ("Y-m-d", MkTime (date("H"),date("i"),date("s"),date("m"),date("d"),date("Y")));
 
 
 if ( $j == 0 )
@@ -1167,7 +1168,7 @@ $riadok=$hlavicka->r90;
 if ( $riadok == 0 ) $riadok="";
   $text = "  <r90><![CDATA[".$riadok."]]></r90>"."\r\n"; fwrite($soubor, $text);
 $riadok=$hlavicka->r91;
-if ( $riadok == 0 ) $riadok="";
+//if ( $riadok == 0 ) $riadok="";
   $text = "  <r91><![CDATA[".$riadok."]]></r91>"."\r\n"; fwrite($soubor, $text);
 $riadok=$hlavicka->r92;
 if ( $riadok == 0 ) $riadok="";
@@ -1194,7 +1195,7 @@ $riadok=$hlavicka->r99;
 if ( $riadok == 0 ) $riadok="";
   $text = "  <r99><![CDATA[".$riadok."]]></r99>"."\r\n"; fwrite($soubor, $text);
 $riadok=$hlavicka->r100;
-if ( $riadok == 0 ) $riadok="";
+//if ( $riadok == 0 ) $riadok="";
   $text = "  <r100><![CDATA[".$riadok."]]></r100>"."\r\n"; fwrite($soubor, $text);
 $riadok=$hlavicka->r101;
 if ( $riadok == 0 ) $riadok="";
@@ -1206,7 +1207,7 @@ $riadok=$hlavicka->r103;
 if ( $riadok == 0 ) $riadok="";
   $text = "  <r103><![CDATA[".$riadok."]]></r103>"."\r\n"; fwrite($soubor, $text);
 $riadok=$hlavicka->r104;
-if ( $riadok == 0 ) $riadok="";
+//if ( $riadok == 0 ) $riadok="";
   $text = "  <r104><![CDATA[".$riadok."]]></r104>"."\r\n"; fwrite($soubor, $text);
 $riadok=$hlavicka->r105;
 if ( $riadok == 0 ) $riadok="";
@@ -1309,11 +1310,11 @@ $neuplatnujem=$hlavicka->upl50;
   $text = "  <neuplatnujem><![CDATA[".$neuplatnujem."]]></neuplatnujem>"."\r\n"; fwrite($soubor, $text);
 $riadok=$hlavicka->spl3d;
   $text = "  <splnam3per><![CDATA[".$riadok."]]></splnam3per>"."\r\n"; fwrite($soubor, $text);
-$riadok=$hlavicka->r123;
+$riadok=$hlavicka->r134;
 if ( $riadok == 0 ) $riadok="";
-  $text = "  <r123><![CDATA[".$riadok."]]></r123>"."\r\n"; fwrite($soubor, $text);
+  $text = "  <r134><![CDATA[".$riadok."]]></r134>"."\r\n"; fwrite($soubor, $text);
 
-  $text = "  <r124>"."\r\n"; fwrite($soubor, $text);
+  $text = "  <r135>"."\r\n"; fwrite($soubor, $text);
 $pico=$hlavicka->pico;
 if ( $hlavicka->pico < 1000000 AND $hlavicka->pico > 1 ) { $pico="00".$hlavicka->pico; }
 //$ico=$hlavicka->pico."/".$hlavicka->psid;
@@ -1341,7 +1342,7 @@ $obec=iconv("CP1250", "UTF-8", $hlavicka->pmes);
 //new2015
 $riadok=$hlavicka->zslu;
   $text = "    <suhlasZaslUdaje><![CDATA[".$riadok."]]></suhlasZaslUdaje>"."\r\n"; fwrite($soubor, $text);
-  $text = "  </r124>"."\r\n"; fwrite($soubor, $text);
+  $text = "  </r135>"."\r\n"; fwrite($soubor, $text);
 
 
   $text = "  <osobitneZaznamy>"."\r\n"; fwrite($soubor, $text);
@@ -1461,7 +1462,7 @@ $zaznamy=iconv("CP1250", "UTF-8", $hlavicka->osob);
 
 $riadok=$hlavicka->pril;
 if ( $riadok == 0 ) $riadok="";
-  $text = "  <r125><![CDATA[".$riadok."]]></r125>"."\r\n"; fwrite($soubor, $text);
+  $text = "  <r136><![CDATA[".$riadok."]]></r136>"."\r\n"; fwrite($soubor, $text);
 
 $datum=SKDatum($hlavicka->dat);
 if ( $datum =='00.00.0000' ) $datum="";
@@ -1835,6 +1836,23 @@ echo "Nie je vyplnen˝ <strong>d·tum vyhl·senia</strong> daÚovÈho priznania.";
 ?>
 </li>
 <li class="orange">
+<?php
+$wrongdat=1;
+$sqlico = mysql_query("SELECT * FROM F$kli_vxcf"."_mzdpriznanie_fob WHERE dat <= '$dat_datsql' ");
+  if (@$zaznam=mysql_data_seek($sqlico,0))
+  {
+  $riadico=mysql_fetch_object($sqlico);
+  $ibanx=$riadico->ibanx;
+  $wrongdat=0;
+  }
+if ( $wrongdat == 1 )
+{
+$upozorni12=1;
+echo "<strong>D·tum vyhl·senia</strong> nemÙûe byù vyööÌ ako aktu·lny d·tum.";
+}
+?>
+</li>
+<li class="orange">
 <?php if ( ( $hlavicka->zdbo == 0 AND $hlavicka->zpre == 0 ) AND ( $hlavicka->post == 1 OR $hlavicka->ucet == 1 OR $hlavicka->diban != "" OR $hlavicka->da2 != '0000-00-00' ) )
 {
 $upozorni12=1;
@@ -1851,23 +1869,103 @@ echo "V <strong>XIV.oddiele ûiadam</strong> o vyplatenie / vr·tenie, ale nie je 
 ?>
 </li>
 <li class="orange">
+<?php
+$wrongdat=1;
+$sqlico = mysql_query("SELECT * FROM F$kli_vxcf"."_mzdpriznanie_fob WHERE da2 <= '$dat_datsql' ");
+  if (@$zaznam=mysql_data_seek($sqlico,0))
+  {
+  $riadico=mysql_fetch_object($sqlico);
+  $ibanx=$riadico->ibanx;
+  $wrongdat=0;
+  }
+if ( $wrongdat == 1 )
+{
+$upozorni12=1;
+echo "<strong>D·tum ûiadosti o vr·tenie</strong> nemÙûe byù vyööÌ ako aktu·lny d·tum.";
+}
+?>
+</li>
+<li class="orange">
+<?php
+$wrongdat=1;
+$sqlico = mysql_query("SELECT * FROM F$kli_vxcf"."_mzdpriznanie_fob WHERE dat <= '$dat_datsql' ");
+  if (@$zaznam=mysql_data_seek($sqlico,0))
+  {
+  $riadico=mysql_fetch_object($sqlico);
+  $ibanx=$riadico->ibanx;
+  $wrongdat=0;
+  }
+if ( $wrongdat == 1 )
+{
+$upozorni12=1;
+echo "<strong>D·tum ûiadosti o vr·tenie</strong> nemÙûe byù vyööÌ ako aktu·lny d·tum.";
+}
+?>
+</li>
+<li class="orange">
 <?php if ( ( $hlavicka->zdbo == 1 OR $hlavicka->zpre == 1 ) AND $hlavicka->ucet == 1 AND $hlavicka->diban == "" ) {
 $upozorni6=1;
 echo "V <strong>XIV.oddiele ûiadam</strong> o vyplatenie / vr·tenie na ˙Ëet, ale nie je vyplnen˝ <strong>IBAN</strong> ˙Ëtu na vyplatenie / vr·tenie.";
 } ?>
 </li>
 </ul>
+
+<ul id="alertpage14" style="display:none;">
+<li class="header-section">STRANA 14 - PRÕLOHA 2</li>
+<li class="orange">
+<?php
+$wrongdat=1;
+$sqlico = mysql_query("SELECT * FROM F$kli_vxcf"."_mzdpriznanie_fob WHERE szdat <= '$dat_datsql' ");
+  if (@$zaznam=mysql_data_seek($sqlico,0))
+  {
+  $riadico=mysql_fetch_object($sqlico);
+  $ibanx=$riadico->ibanx;
+  $wrongdat=0;
+  }
+if ( $wrongdat == 1 )
+{
+$upozorni14=1;
+echo "<strong>D·tum v prÌlohe 2</strong> nemÙûe byù vyööÌ ako aktu·lny d·tum.";
+}
+?>
+</li>
+<li class="orange">
+<?php if ( $hlavicka->sz9 != 0 AND $hlavicka->szdat == '0000-00-00' )
+{
+$upozorni14=1;
+echo "Je vyplnen˝ riadok 9, ale ch˝ba vyplnen˝ <strong>d·tum</strong>.";
+}
+?>
+</li>
+<li class="orange">
+<?php if ( $hlavicka->r35 != $hlavicka->sz9 )
+{
+$upozorni14=1;
+echo "Riadok 35 zo strany 2 sa musÌ rovnaù <strong>riadku 9</strong> v PrÌlohe 2 priznania.";
+}
+?>
+</li>
+<li class="orange">
+<?php if ( $hlavicka->psp6 != ( $hlavicka->sz12 + $hlavicka->sz14 ) )
+{
+$upozorni14=1;
+echo "Hodnota v riadku \"preuk·zateænÈ zaplatenÈ poistnÈ\" na strane 3 sa musÌ rovnaù <strong>riadok 12 + 14</strong> v PrÌlohe 2 priznania.";
+}
+?>
+</li>
+</ul>
 </div> <!-- #upozornenie -->
 
 <script type="text/javascript">
 <?php
-if ( $upozorni1 == 1 OR $upozorni2 == 1 OR $upozorni10 == 1 OR $upozorni11 == 1 OR $upozorni12 == 1 )
+if ( $upozorni1 == 1 OR $upozorni2 == 1 OR $upozorni10 == 1 OR $upozorni11 == 1 OR $upozorni12 == 1 OR $upozorni14 == 1 )
      { echo "upozornenie.style.display='block';"; }
 if ( $upozorni1 == 1 ) { echo "alertpage1.style.display='block';"; } 
 if ( $upozorni2 == 1 ) { echo "alertpage2.style.display='block';"; } 
 if ( $upozorni10 == 1 ) { echo "alertpage10.style.display='block';"; }
 if ( $upozorni11 == 1 ) { echo "alertpage11.style.display='block';"; }
 if ( $upozorni12 == 1 ) { echo "alertpage12.style.display='block';"; }
+if ( $upozorni14 == 1 ) { echo "alertpage14.style.display='block';"; }
 ?>
 </script>
 
