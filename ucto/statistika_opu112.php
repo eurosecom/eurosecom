e4ca027baece516965877df92d73dee68b96d6c7
61Stan <stanislav61@gmail.com>
2017-12-04 16:09:05 +0100

    OPU 112 v17
    #175
    - obdobie na 1.strane cez premennú

--- ucto/statistika_opu112.php
+++ ucto/statistika_opu112.php
@@ -251,8 +251,8 @@ $sqldok = mysql_query("SELECT * FROM F$kli_vxcf"."_treximafir ");
   }
 
 //mesiac
-$mesiacx=$mesiac;
-if ( $mesiacx < 10 ) { $mesiacx="0".$mesiacx; }
+$kli_vrokx = substr($kli_vrok,2,2);
+$mesiacx=$mesiac; if ( $mesiacx < 10 ) { $mesiacx="0".$mesiacx; }
 
 //6-miestne ico
 $fir_ficox=$fir_fico; if ( $fir_ficox < 999999 ) { $fir_ficox="00".$fir_ficox; }
@@ -387,7 +387,8 @@ $source="statistika_opu112.php?drupoh=1&page=1";
 
 <?php if ( $strana == 1 OR $strana == 9999 ) { ?>
 <img src="<?php echo $jpg_cesta; ?>_str1.jpg" class="form-background" alt="<?php echo $jpg_popis; ?> 1.strana 302kB">
-
+<span class="text-echo" style="top:250px; left:480px; font-size:24px; letter-spacing:0.02em;"><?php echo $kli_vrok; ?></span>
+<span class="text-echo" style="top:370px; left:196px; font-size:18px; letter-spacing:24px;"><?php echo $kli_vrokx; ?></span>
 <span class="text-echo" style="top:370px; left:263px; font-size:18px; letter-spacing:24px;"><?php echo $mesiacx; ?></span>
 <span class="text-echo" style="top:370px; left:332px; font-size:18px; letter-spacing:24px;"><?php echo $fir_ficox; ?></span>
 <!-- ORGANIZACIA -->
@@ -479,12 +480,21 @@ if ( File_Exists($jpg_cesta.'_str1.jpg') AND $i == 0 )
 $pdf->Image($jpg_cesta.'_str1.jpg',0,0,210,297);
 }
 $pdf->SetY(10);
+//za rok
+$pdf->SetFont('arial','',16);
+$pdf->Cell(190,41," ","$rmc1",1,"L");
+$pdf->Cell(95,6," ","$rmc1",0,"L");$pdf->Cell(16,7,"$kli_vrok","$rmc",1,"C");
+$pdf->SetFont('arial','',12);
 
+//rok
+$A=substr($kli_vrokx,0,1);
+$B=substr($kli_vrokx,1,1);
+$pdf->Cell(190,19," ","$rmc1",1,"L");
+$pdf->Cell(31,6," ","$rmc1",0,"L");$pdf->Cell(7,8,"$A","$rmc",0,"C");$pdf->Cell(8,8,"$B","$rmc",0,"C");
 //mesiac
 $A=substr($mesiacx,0,1);
 $B=substr($mesiacx,1,1);
-$pdf->Cell(190,67," ","$rmc1",1,"L");
-$pdf->Cell(46,6," ","$rmc1",0,"L");$pdf->Cell(7,8,"$A","$rmc",0,"C");$pdf->Cell(8,8,"$B","$rmc",0,"C");
+$pdf->Cell(7,8,"$A","$rmc",0,"C");$pdf->Cell(8,8,"$B","$rmc",0,"C");
 //ico
 $A=substr($fir_ficox,0,1);
 $B=substr($fir_ficox,1,1);
