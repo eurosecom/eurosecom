833433ce49fb534f2ac47ff17b37a3d43a9d7d23
Stanislav Kubík <stanislav61@gmail.com>
2018-02-25 13:00:50 +0100

    FIN 3-04 v16
    #132
    - zmenená 1.strana pre rok 2018

--- ucto/vykaz_fin304_2016.php
+++ ucto/vykaz_fin304_2016.php
@@ -27,7 +27,12 @@ $rmc=0;
 $rmc1=0;
 
 //.jpg podklad
+if ( $kli_vrok < 2018 ) {
 $jpg_cesta="../dokumenty/statistika2016/fin304/fin3-04_v16";
+                        }
+if ( $kli_vrok >= 2018 ) {
+$jpg_cesta="../dokumenty/tlacivo2018/fin3-04/fin3-04_v18";
+                        }
 $jpg_popis="Finanèný výkaz o finanèných aktívach pod¾a sektorov subjektu verejnej správy FIN 3-04 za rok ".$kli_vrok;
 
 $pole = explode(".", $kli_vume);
@@ -992,14 +997,8 @@ img.btn-form-tool {
   }
   function MetodVypln()
   {
-<?php if ( $kli_vrok < 2018 ) { ?>
    window.open('<?php echo $jpg_cesta; ?>_vysvetlivky.pdf',
 '_blank', 'width=980, height=900, top=0, left=20, status=yes, resizable=yes, scrollbars=yes, menubar=yes, toolbar=yes');
-<?php              } ?>
-<?php if ( $kli_vrok >= 2018 ) { ?>
-   window.open('../dokumenty/tlacivo2018/fin3-04/fin3-04_v18_vysvetlivky.pdf',
-'_blank', 'width=980, height=900, top=0, left=20, status=yes, resizable=yes, scrollbars=yes, menubar=yes, toolbar=yes');
-<?php              } ?>
   }
   function TlacVykaz()
   {
