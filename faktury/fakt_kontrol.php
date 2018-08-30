dc8f596d9a9b3f2cc62bd62018d6113c050f4f9e
eurosecom <eurosecom1@gmail.com>
2018-08-30 15:23:51 +0200

    Odbyt Ploty Skala
    #223
    - kontrola duplicity TEST podľa rozdielu dátumov

--- faktury/fakt_kontrol.php
+++ faktury/fakt_kontrol.php
@@ -44,9 +44,12 @@ $sqlt = <<<prcprizdphs
    ume          FLOAT(8,4),
    dat          DATE,
    dok          INT,
+   dal          DATE,
+   dol          INT,
    hod          DECIMAL(10,2),
    fak          DECIMAL(10,0),
    ico          DECIMAL(10,0),
+   dni          DECIMAL(10,0),
    fic          INT
 );
 prcprizdphs;
@@ -128,19 +131,30 @@ $oznac = mysql_query("$sqtoz");
 //er1	psys	ume	dat	dok	hod	fak ico	fic
 
 $dsqlt = "INSERT INTO F$kli_vxcf"."_prcprizdphs$kli_uzid"." SELECT".
-" 0,1,uce,ume,dat,dok,0,0,ico,0 FROM F$kli_vxcf"."_fakodb WHERE dok >= 0 ";
+" 0,1,uce,ume,dat,dok,'',0,0,0,ico,0,0 FROM F$kli_vxcf"."_fakodb WHERE dok >= 0 ";
 $dsql = mysql_query("$dsqlt");
 
 $dsqlt = "INSERT INTO F$kli_vxcf"."_prcprizdphs$kli_uzid"." SELECT".
-" 0,11,0,ume,dat,0,0,dok,ico,0 FROM F$kli_vxcf"."_fakdol WHERE dok >= 0 AND fak = 0 ";
+" 0,11,0,ume,'',0,dat,dok,0,0,ico,0,0 FROM F$kli_vxcf"."_fakdol WHERE dok >= 0 ";
 $dsql = mysql_query("$dsqlt");
 
 $dsqlt = "INSERT INTO F$kli_vxcf"."_prcprizdphs$kli_uzid"." SELECT".
-" 0,1,MAX(uce),MAX(ume),MAX(dat),MAX(dok),hod,MAX(fak),ico,99 FROM F$kli_vxcf"."_prcprizdphs$kli_uzid"." WHERE dok >= 0 GROUP BY ico";
+" 0,1,MAX(uce),MAX(ume),MAX(dat),MAX(dok),MAX(dal),MAX(dol),hod,MAX(fak),ico,0,99 FROM F$kli_vxcf"."_prcprizdphs$kli_uzid"." WHERE dok >= 0 GROUP BY ico";
 $dsql = mysql_query("$dsqlt");
 
-//nastav eror ak je fak aj dod za ico
-$sqtoz = "UPDATE F$kli_vxcf"."_prcprizdphs$kli_uzid SET er1=1 WHERE fic = 99 AND dok != 0 AND fak != 0 ";
+//exit;
+
+$sqtoz = "DELETE FROM F$kli_vxcf"."_prcprizdphs$kli_uzid WHERE fic != 99";
+$oznac = mysql_query("$sqtoz");
+
+$sqtoz = "DELETE FROM F$kli_vxcf"."_prcprizdphs$kli_uzid WHERE dok = 0 OR dol = 0 ";
+$oznac = mysql_query("$sqtoz");
+
+$sqtoz = "UPDATE F$kli_vxcf"."_prcprizdphs$kli_uzid SET dni=datediff(dat, dal) WHERE dok != 0 AND dol != 0 ";
+$oznac = mysql_query("$sqtoz"); 
+
+//nastav eror ak je fak aj dod za ico a datumy su < 60
+$sqtoz = "UPDATE F$kli_vxcf"."_prcprizdphs$kli_uzid SET er1=1 WHERE fic = 99 AND dok != 0 AND dol != 0 ";
 $oznac = mysql_query("$sqtoz"); 
 
 //exit;
@@ -239,8 +253,11 @@ if( $copern == 1 AND $drupoh == 1) //html;
 <td class="bmenu" width="10%">Úè.mes.</td>
 <td class="bmenu" width="10%">Dátum</td>
 <td class="bmenu" width="10%" align="right">Faktúra</td>
+<td class="bmenu" width="10%">Dátum</td>
 <td class="bmenu" width="10%" align="right">Dodací list</td>
-<td class="bmenu" width="60%">IÈO</td>
+<td class="bmenu" width="5%">Dni</td>
+<td class="bmenu" width="45%">IÈO</td>
+
 </tr>
 <?php
    }
@@ -259,9 +276,9 @@ if( $hlavicka->fak == 0 ) $faknul="";
   list ($rok, $mes, $den) = split ('[-]', $hlavicka->dat, 3);
   $rok=$rok-2000;
   $datsk = sprintf("%02d.%02d.%02d", $den, $mes, $rok);
-  list ($rok, $mes, $den) = split ('[-]', $hlavicka->daz, 3);
+  list ($rok, $mes, $den) = split ('[-]', $hlavicka->dal, 3);
   $rok=$rok-2000;
-  $dazsk = sprintf("%02d.%02d.%02d", $den, $mes, $rok);
+  $dalsk = sprintf("%02d.%02d.%02d", $den, $mes, $rok);
 
 
 if( $copern == 1 AND $drupoh == 1 )
@@ -291,11 +308,13 @@ if( $hlavicka->er1 > 0 ) { $hvstup="hvstup_bred"; }
 <img src='../obr/uprav.png' width=15 height=15 onClick="upravFakt(<?php echo $hlavicka->uce; ?>, <?php echo $hlavicka->dok; ?>);" border=1 title="Úprava faktúry" >
 
 </td>
-
+<td class="<?php echo $hvstup; ?>" ><?php echo $dalsk; ?></td>
 <td class="<?php echo $hvstup; ?>" align="right" >
 
-<?php echo $hlavicka->fak; ?>
+<?php echo $hlavicka->dol; ?>
 <img src='../obr/orig.png' width=15 height=15 onClick="upravDod(<?php echo $hlavicka->fak; ?>);" border=1 title="Úprava dodacieho listu" >
+
+<td class="<?php echo $hvstup; ?>" ><?php echo $hlavicka->dni; ?></td>
 
 </td>
 <td class="<?php echo $hvstup; ?>" ><?php echo $hlavicka->ico; ?> <?php echo $hlavicka->nai; ?> <?php echo $hlavicka->mes; ?></td>
