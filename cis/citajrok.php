e216c91b2febdc401cf1e238a070e63f38a83c29
eurosecom <eurosecom1@gmail.com>
2018-11-12 19:05:44 +0100

    Drobné úpravy
    - CIS na enposro.sk len roky od 2017 vrátane, ostatné na eszalohy.sk

--- cis/citajrok.php
+++ cis/citajrok.php
@@ -3,7 +3,7 @@
 //ak je eszalohy.sk tak citaj len vyssi rok z fir
 $citajrok=0;
 if( $_SERVER['SERVER_NAME'] == "localhost" ) { $citajrok=2014; }
-if( $_SERVER['SERVER_NAME'] == "www.enposro.sk" ) { $citajrok=2014; }
+if( $_SERVER['SERVER_NAME'] == "www.enposro.sk" ) { $citajrok=2016; }
 if( $_SERVER['SERVER_NAME'] == "www.eurosecom.sk" ) { $citajrok=2014; }
 
 ?>
